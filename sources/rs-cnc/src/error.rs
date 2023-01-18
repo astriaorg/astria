3b0e8f0a09fc48e02484c3d27491c39a36b7f6e8
Jesse Snyder <jessetsnyder@gmail.com>
2023-01-18 15:10:27 -0700

    Update error.rs

--- sources/rs-cnc/src/error.rs
+++ sources/rs-cnc/src/error.rs
@@ -2,6 +2,7 @@ use thiserror::Error;
 
 #[derive(Error, Debug, Clone)]
 pub enum CelestiaNodeClientError {
+    /// An error from our Reqwest http client
     #[error("Reqwest: {0}")]
     HttpClient(String),
 }
