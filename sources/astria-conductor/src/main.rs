27da0ab113b6afb436b05e530a92f10522d2848f
Jesse Snyder <jessetsnyder@gmail.com>
2023-01-24 12:55:43 -0700

    comments

--- sources/astria-conductor/src/main.rs
+++ sources/astria-conductor/src/main.rs
@@ -7,6 +7,7 @@ mod error;
 
 #[tokio::main]
 async fn main() {
+    // log to file and stderr
     flexi_logger::Logger::try_with_str("info")
         .unwrap()
         .log_to_file(FileSpec::default().directory("/tmp/astria-rv-rs"))
@@ -14,13 +15,11 @@ async fn main() {
         .start()
         .unwrap();
 
+    // PoC usage of driver and driver_cmd_tx
     let (mut driver, driver_cmd_tx) = driver::Driver::new().unwrap();
     let _ = task::spawn(async move { driver.run().await });
-
     driver_cmd_tx.send(driver::DriverCommand::GetNewBlocks { last_block_height: 53 }).ok();
-
     // NOTE - fake some computation time so the previous command has time to run before shutdown
     thread::sleep(time::Duration::from_secs(1));
-
     driver_cmd_tx.send(driver::DriverCommand::Shutdown).ok();
 }
