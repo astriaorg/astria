8b1993bacfca77502a77c2f88c1aeeff3d07c7ff
Jesse Snyder <jessetsnyder@gmail.com>
2023-01-26 11:04:47 -0700

    cleanup

--- sources/astria-conductor/src/logger.rs
+++ sources/astria-conductor/src/logger.rs
@@ -2,7 +2,6 @@ use flexi_logger::{DeferredNow, Duplicate, FileSpec};
 
 pub fn initialize() -> () {
     // log to file and stderr
-    // TODO - move to own module
     flexi_logger::Logger::try_with_str("info")
         .unwrap()
         .log_to_file(FileSpec::default().directory("/tmp/astria-rv-rs"))
