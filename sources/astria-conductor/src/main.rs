44ed8a5a86ed301cb27a61b0de762aba080cae45
Jesse Snyder <jessetsnyder@gmail.com>
2023-01-31 11:28:59 -0700

    make namespace_id cli arg

--- sources/astria-conductor/src/main.rs
+++ sources/astria-conductor/src/main.rs
@@ -13,25 +13,34 @@ mod reader;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // cli args
     // TODO - move to own module
     let url_option = Arg::new("url")
         .short('u')
-        .help("URL of the data layer.")
+        .help("URL of the data layer server.")
+        .required(true);
+    let namespace_id_option = Arg::new("namespace_id")
+        .short('n')
+        .help("Namespace ID as a string; the hex encoding of a [u8; 8]")
         .required(true);
     let cli_app = Command::new("rv-rs")
         .version("0.1")
-        .about("A cli to read and write blocks from and to different sources.")
-        .arg(url_option);
-    let matches = cli_app.get_matches();
+        .about("A cli to read and write blocks from and to different sources. Uses the Actor model.")
+        .arg(url_option)
+        .arg(namespace_id_option);
 
-    // TODO - namespace id?
-    let base_url = matches.get_one::<String>("url").expect("url required");
+    let matches = cli_app.get_matches();
+    let base_url = matches
+        .get_one::<String>("url")
+        .expect("url required");
+    let namespace_id = matches
+        .get_one::<String>("namespace_id")
+        .expect("namespace id required");
 
     // logs
     logger::initialize();
 
     // configuration
-    let namespace_id = String::from("b860ccf0e97fdf6c");
     let conf = Conf::new(base_url.to_owned(), namespace_id.to_owned());
 
     log::info!("Using node at {}", conf.celestia_node_url);
