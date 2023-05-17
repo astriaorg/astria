6b67fcbb6f76ec15feb1346bfb8c0c97f4ab5aaa
itamar <itamar.reif@gmail.com>
2023-05-17 16:39:19 -0400

    super qualifiers compilation error

--- crates/astria-proto/src/lib.rs
+++ crates/astria-proto/src/lib.rs
@@ -1,3 +1,7 @@
 pub mod execution {
     include!(concat!(env!("OUT_DIR"), "/astria.execution.v1.rs"));
 }
+
+pub mod sequencer_relayer {
+    include!(concat!(env!("OUT_DIR"), "/astria.sequencer_relayer.v1.rs"));
+}
