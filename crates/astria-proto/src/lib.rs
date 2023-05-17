4c09b11f307ad7b29068ac52bc0eee66db7791d7
Jordan Oroshiba <jordan.oroshiba@gmail.com>
2023-05-17 11:14:43 -0700

    Consolidating proto definitions in astria-proto, and add CI tests

---
+++ crates/astria-proto/src/lib.rs
@@ -0,0 +1,12 @@
+pub mod execution {
+    pub mod v1 {
+        include!(concat!(env!("OUT_DIR"), "/astria.execution.v1.rs"));
+    }
+    
+}
+
+pub mod sequencer {
+    pub mod v1 {
+        include!(concat!(env!("OUT_DIR"), "/astria.sequencer.v1.rs"));
+    }
+}
