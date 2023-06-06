776273b6c5f4b834f66d75d407281bd2243224cc
elizabeth <elizabethjbinks@gmail.com>
2023-06-06 18:02:30 -0400

    add UintString and IntString types for serde

--- crates/astria-conductor/src/tendermint.rs
+++ crates/astria-conductor/src/tendermint.rs
@@ -10,9 +10,14 @@ use reqwest::{
     Response as ReqwestResponse,
 };
 use serde::{
-    de::DeserializeOwned,
+    de::{
+        DeserializeOwned,
+        Error,
+    },
     Deserialize,
+    Deserializer,
     Serialize,
+    Serializer,
 };
 
 static VALIDATOR_SET_ENDPOINT: &str = "/cosmos/base/tendermint/v1beta1/validatorsets/";
@@ -31,12 +36,8 @@ impl ValidatorSet {
     /// the validator with the highest proposer priority is the proposer.
     /// TODO: could there ever be two validators with the same priority?
     pub(crate) fn get_proposer(&mut self) -> eyre::Result<Validator> {
-        self.validators.sort_by(|v1, v2| {
-            v1.proposer_priority
-                .parse::<i64>()
-                .unwrap()
-                .cmp(&v2.proposer_priority.parse::<i64>().unwrap())
-        });
+        self.validators
+            .sort_by(|v1, v2| v1.proposer_priority.0.cmp(&v2.proposer_priority.0));
         self.validators
             .first()
             .cloned()
@@ -48,8 +49,8 @@ impl ValidatorSet {
 pub struct Validator {
     pub address: String,
     pub pub_key: KeyWithType,
-    pub voting_power: String,
-    pub proposer_priority: String,
+    pub voting_power: UintString,
+    pub proposer_priority: IntString,
 }
 
 #[derive(Deserialize, Debug, Clone)]
@@ -98,5 +99,45 @@ impl TendermintClient {
             .json::<Resp>()
             .await
             .wrap_err("failed reading server response as json")
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct UintString(pub u64);
+
+impl<'de> Deserialize<'de> for UintString {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        Ok(UintString(
+            String::deserialize(deserializer)?
+                .parse::<i64>()
+                .map_err(|e| D::Error::custom(format!("{e}")))?
+                .try_into()
+                .map_err(|e| D::Error::custom(format!("{e}")))?,
+        ))
+    }
+}
+
+impl Serialize for UintString {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        self.0.to_string().serialize(serializer)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct IntString(pub i64);
+
+impl<'de> Deserialize<'de> for IntString {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        Ok(IntString(
+            String::deserialize(deserializer)?
+                .parse::<i64>()
+                .map_err(|e| D::Error::custom(format!("{e}")))?,
+        ))
+    }
+}
+
+impl Serialize for IntString {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        self.0.to_string().serialize(serializer)
     }
 }
