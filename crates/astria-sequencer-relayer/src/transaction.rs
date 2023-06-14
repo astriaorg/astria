d7cc62bcf547c13cbb014f12264382e173b55aa0
itamar <itamar.reif@gmail.com>
2023-06-13 22:56:02 -0400

    clean up transaction.rs

--- crates/astria-sequencer-relayer/src/transaction.rs
+++ crates/astria-sequencer-relayer/src/transaction.rs
@@ -4,12 +4,6 @@ use tendermint::{
     merkle,
 };
 
-fn tx_to_prost_bytes(tx: Vec<u8>) -> prost::alloc::vec::Vec<u8> {
-    let mut buf = prost::alloc::vec::Vec::new();
-    prost::encoding::bytes::encode(1, &tx, &mut buf);
-    buf
-}
-
 pub fn txs_to_data_hash(txs: &Vec<Vec<u8>>) -> TmHash {
     TmHash::Sha256(merkle::simple_hash_from_byte_vectors::<
         tendermint::crypto::default::Sha256,
