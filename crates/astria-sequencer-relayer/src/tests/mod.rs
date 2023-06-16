a8560388118473c54b38937fbaa01ca4b8a9c4e7
itamar <itamar.reif@gmail.com>
2023-06-16 14:10:45 -0400

    fix header test

--- crates/astria-sequencer-relayer/src/tests/mod.rs
+++ crates/astria-sequencer-relayer/src/tests/mod.rs
@@ -3,6 +3,7 @@ use tendermint::{
     Block,
     Hash,
 };
+use tendermint_proto::Protobuf;
 
 use crate::sequencer::SequencerClient;
 
@@ -14,7 +15,7 @@ async fn test_header_to_tendermint_header() {
     let client = SequencerClient::new(sequencer_endpoint).unwrap();
 
     let resp = client.get_latest_block().await.unwrap();
-    let block_id_hash = Hash::try_from(resp.block_id.hash).unwrap();
+    let block_id_hash = Hash::decode_vec(&resp.block_id.hash.0).unwrap();
     let block = Block::try_from(resp.block).unwrap();
     let tm_header_hash = block.header.hash();
     assert_eq!(tm_header_hash, block_id_hash);
