daa45e50c37e593fbd856b7ef7df6a911540971a
Sam Bukowski <sam.c.bukowski@gmail.com>
2023-06-29 14:22:18 -0600

    converted from anyhow to eyre

--- crates/astria-sequencer-utils/src/genesis_parser.rs
+++ crates/astria-sequencer-utils/src/genesis_parser.rs
@@ -3,9 +3,9 @@ use std::{
     path::Path,
 };
 
-use anyhow::{
-    Context as _,
+use eyre::{
     Result,
+    WrapErr,
 };
 use serde_json::{
     to_writer_pretty,
@@ -26,31 +26,32 @@ impl GenesisParser {
         );
         // load sequencer genesis data
         let source_genesis_file_path = File::open(data.source_genesis_file)
-            .context("failed to open sequencer genesis file")?;
+            .wrap_err("failed to open sequencer genesis file")?;
         let source_genesis_data: Value = serde_json::from_reader(&source_genesis_file_path)
-            .context("failed deserializing sequencer genesis state from file")?;
+            .wrap_err("failed deserializing sequencer genesis state from file")?;
 
         // load cometbft genesis data
         let destination_genesis_file_path = File::open(data.destination_genesis_file.as_str())
-            .context("failed to open cometbft genesis file")?;
+            .wrap_err("failed to open cometbft genesis file")?;
         let mut destination_genesis_data: Value =
             serde_json::from_reader(&destination_genesis_file_path)
-                .context("failed deserializing cometbft genesis state from file")?;
+                .wrap_err("failed deserializing cometbft genesis state from file")?;
 
         // merge sequencer genesis data into cometbft genesis data
         merge_json(&mut destination_genesis_data, &source_genesis_data);
 
         // write new state
         let dest_file = File::create(Path::new(data.destination_genesis_file.as_str()))
-            .context("failed to open destination genesis json file")?;
-        to_writer_pretty(dest_file, &destination_genesis_data)?;
+            .wrap_err("failed to open destination genesis json file")?;
+        to_writer_pretty(dest_file, &destination_genesis_data)
+            .wrap_err("failed to write to output json file")?;
 
         Ok(())
     }
 }
 
 /// Merges a source JSON Value into a destination JSON Value.
-// inpiration for this function came from: https://stackoverflow.com/questions/47070876/how-can-i-merge-two-json-objects-with-rust
+// context: https://stackoverflow.com/questions/47070876/how-can-i-merge-two-json-objects-with-rust
 fn merge_json(a: &mut Value, b: &Value) {
     match (a, b) {
         (Value::Object(a), Value::Object(b)) => {
