3235fbbce5bdab8e22988615b098e0b5255a77a6
elizabeth <elizabethjbinks@gmail.com>
2023-07-04 18:10:42 -0400

    lint

--- crates/astria-sequencer/src/lib.rs
+++ crates/astria-sequencer/src/lib.rs
@@ -5,7 +5,7 @@ pub(crate) mod component;
 pub mod config;
 pub(crate) mod crypto;
 pub(crate) mod genesis;
-pub(crate) mod sequence;
+pub mod sequence;
 mod sequencer;
 pub(crate) mod service;
 pub(crate) mod state_ext;
