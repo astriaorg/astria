5a0e24e2ecdcb402a35e1fae0017df82ebc93cfe
Richard Janis Goldschmidt <github@aberrat.io>
2023-07-04 10:12:20 +0200

    feat(sequencer-relayer): single entry point (#89)
    This commit adds a `SequencerRelayer` type to
    `astria-sequencer-relayer`, which serves as the
    single entrypoint to the service. It creates all
    constituent tasks from the service config, and
    runs them until stopped.
    
    This also lays the groundwork for writing
    integration tests against `SequencerRelayer`
    using the same codepaths as are used by the
    binary.
    
    In future commits all items that are currently
    exposed will be made crate-private, except for
    the `SequencerRelayer` type itself, config
    generation, and telemetry initialization.
    
    Because other workspace crates depend on items
    defined within `astria-sequencer-relayer` they
    are currently left unchanged.

--- crates/astria-sequencer-relayer/src/main.rs
+++ crates/astria-sequencer-relayer/src/main.rs
@@ -1,23 +1,15 @@
-use std::{
-    net::SocketAddr,
-    time,
-};
-
 use astria_sequencer_relayer::{
-    api,
     config,
-    data_availability::CelestiaClientBuilder,
-    network::GossipNetwork,
-    relayer::Relayer,
-    sequencer::SequencerClient,
+    SequencerRelayer,
 };
+use eyre::WrapErr as _;
 use tracing::{
     info,
     warn,
 };
 
 #[tokio::main]
-async fn main() {
+async fn main() -> eyre::Result<()> {
     let cfg = config::get().expect("failed to read configuration");
     tracing_subscriber::fmt().with_env_filter(&cfg.log).init();
     let cfg_json = serde_json::to_string(&cfg).unwrap_or_else(|e| {
@@ -27,37 +19,12 @@ async fn main() {
         );
         format!("{cfg:?}")
     });
-    info!(config = cfg_json, "running astria-sequencer-relayer");
+    info!(config = cfg_json, "starting astria-sequencer-relayer");
 
-    let sequencer_client = SequencerClient::new(cfg.sequencer_endpoint.clone())
-        .expect("failed to create sequencer client");
-    let da_client = CelestiaClientBuilder::new(cfg.celestia_endpoint.clone())
-        .gas_limit(cfg.gas_limit)
-        .build()
-        .expect("failed to create data availability client");
+    SequencerRelayer::new(&cfg)
+        .wrap_err("failed to initialize sequencer relayer")?
+        .run()
+        .await;
 
-    let sleep_duration = time::Duration::from_millis(cfg.block_time);
-    let interval = tokio::time::interval(sleep_duration);
-
-    let (block_tx, block_rx) = tokio::sync::mpsc::unbounded_channel();
-
-    let network = GossipNetwork::new(cfg.p2p_port, block_rx).expect("failed to create network");
-    let network_handle = network.run();
-
-    let mut relayer = Relayer::new(cfg.clone(), sequencer_client, da_client, interval, block_tx)
-        .expect("failed to create relayer");
-
-    if cfg.disable_writing {
-        relayer.disable_writing();
-    }
-
-    let relayer_state = relayer.subscribe_to_state();
-    let relayer_handle = relayer.run();
-
-    let _api_server_task = tokio::task::spawn(async move {
-        let api_addr = SocketAddr::from(([127, 0, 0, 1], cfg.rpc_port));
-        api::start(api_addr, relayer_state).await;
-    });
-
-    tokio::try_join!(relayer_handle, network_handle).expect("failed to join tasks");
+    Ok(())
 }
