8c79154b2eff770d8e5013932d9d22a9c915c106
Janis Goldschmidt <github@aberrat.io>
2023-07-18 14:09:15 +0200

    report which config is used immediately after starting

--- crates/astria-composer/src/main.rs
+++ crates/astria-composer/src/main.rs
@@ -9,10 +9,12 @@ use tracing::info;
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     let cfg = config::get().expect("failed to read configuration");
+    let cfg_ser = serde_json::to_string(&cfg)
+        .expect("the json serializer should never fail when serializing to a string");
+    eprintln!("config:\n{cfg_ser}");
     telemetry::init(std::io::stdout, &cfg.log).expect("failed to initialize tracing");
 
-    info!(?cfg, "starting astria-composer");
-    // let composer = Composer::new(&cfg);
+    info!(config = cfg_ser, "initializing composer",);
 
     let _searcher = Searcher::new(&cfg).await?.run().await;
 
