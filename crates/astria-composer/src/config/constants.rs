5fe477f10e7abe95206d0df3665b52ac7e8b4a46
Janis Goldschmidt <github@aberrat.io>
2023-07-19 17:09:45 +0200

    also make the default sequencer url a proper url

--- crates/astria-composer/src/config/constants.rs
+++ crates/astria-composer/src/config/constants.rs
@@ -1,5 +1,5 @@
 pub const DEFAULT_API_PORT: u16 = 8080;
-pub const DEFAULT_SEQUENCER_URL: &str = "sequencer.astria.localdev.me";
+pub const DEFAULT_SEQUENCER_URL: &str = "http://sequencer.astria.localdev.me";
 pub const DEFAULT_SEQUENCER_ADDRESS: &str = "1c0c490f1b5528d8173c5de46d131160e4b2c0c3";
 pub const DEFAULT_SEQUENCER_SECRET: &str =
     "2bd806c97f0e00af1a1fc3328fa763a9269723c8db8fac4f93af71db186d6e90";
