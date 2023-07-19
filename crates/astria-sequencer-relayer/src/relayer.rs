820dddfadf71c969d4d7a78f1ceb0de8dc6f3eb5
Richard Janis Goldschmidt <github@aberrat.io>
2023-07-19 08:18:43 +0200

    fix previous autofmt

--- crates/astria-sequencer-relayer/src/relayer.rs
+++ crates/astria-sequencer-relayer/src/relayer.rs
@@ -1,18 +1,35 @@
 use std::time::Duration;
 
-use eyre::{bail, Result, WrapErr as _};
+use eyre::{
+    bail,
+    Result,
+    WrapErr as _,
+};
 use humantime::format_duration;
-use tendermint_rpc::{endpoint::block, HttpClient};
+use tendermint_rpc::{
+    endpoint::block,
+    HttpClient,
+};
 use tokio::{
     select,
-    sync::{mpsc::UnboundedSender, watch},
+    sync::{
+        mpsc::UnboundedSender,
+        watch,
+    },
     task,
     time::interval,
 };
-use tracing::{debug, instrument, warn};
+use tracing::{
+    debug,
+    info,
+    instrument,
+    warn,
+};
 
 use crate::{
-    data_availability::CelestiaClient, macros::report_err, types::SequencerBlockData,
+    data_availability::CelestiaClient,
+    macros::report_err,
+    types::SequencerBlockData,
     validator::Validator,
 };
 
@@ -244,7 +261,10 @@ impl Relayer {
         delay: Duration,
         factor: f32,
     ) -> eyre::Result<()> {
-        use backon::{ExponentialBuilder, Retryable as _};
+        use backon::{
+            ExponentialBuilder,
+            Retryable as _,
+        };
         if let Some(client) = self.data_availability.clone() {
             debug!("attempting to connect to data availability layer",);
             let backoff = ExponentialBuilder::default()
@@ -292,7 +312,10 @@ impl Relayer {
         delay: Duration,
         factor: f32,
     ) -> eyre::Result<()> {
-        use backon::{ExponentialBuilder, Retryable as _};
+        use backon::{
+            ExponentialBuilder,
+            Retryable as _,
+        };
         use tendermint_rpc::Client as _;
 
         debug!("attempting to connect to data availability layer",);
