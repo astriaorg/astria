7cbfe6e325081d9ff9dd6286be985e8598923539
Janis Goldschmidt <github@aberrat.io>
2023-08-03 17:54:46 +0200

    add config tests ensuring example is in sync

--- crates/astria-composer/src/config.rs
+++ crates/astria-composer/src/config.rs
@@ -22,8 +22,9 @@ pub fn get() -> Result<Config, figment::Error> {
     Config::from_environment()
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq)]
 /// The high-level config for creating an astria-composer service.
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+#[serde(deny_unknown_fields)]
 pub struct Config {
     /// Log level. One of debug, info, warn, or error
     pub log: String,
@@ -54,5 +55,41 @@ impl Config {
             .merge(rust_log)
             .merge(Env::prefixed("ASTRIA_COMPOSER_"))
             .extract()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use figment::Jail;
+
+    use super::Config;
+    const EXAMPLE_ENV: &str = include_str!("../example.env");
+
+    fn populate_environment_from_example(jail: &mut Jail) {
+        for line in EXAMPLE_ENV.lines() {
+            if let Some((key, val)) = line.trim().split_once("=") {
+                jail.set_env(key, val);
+            }
+        }
+    }
+
+    #[test]
+    fn ensure_example_env_is_in_sync() {
+        Jail::expect_with(|jail| {
+            populate_environment_from_example(jail);
+            Config::from_environment().unwrap();
+            Ok(())
+        })
+    }
+
+    #[test]
+    #[should_panic]
+    fn extra_env_vars_are_rejected() {
+        Jail::expect_with(|jail| {
+            populate_environment_from_example(jail);
+            jail.set_env("ASTRIA_COMPOSER_FOOBAR", "BAZ");
+            Config::from_environment().unwrap();
+            Ok(())
+        })
     }
 }
