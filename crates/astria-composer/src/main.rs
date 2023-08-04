3a22934fa0c79398129a17da1ee80210ce784dbc
Janis Goldschmidt <github@aberrat.io>
2023-08-04 18:22:29 +0200

    gracefully exit if config could not be read

--- crates/astria-composer/src/main.rs
+++ crates/astria-composer/src/main.rs
@@ -3,12 +3,17 @@ use astria_composer::{
     telemetry,
     Composer,
 };
-use color_eyre::eyre;
 use tracing::info;
 
 #[tokio::main]
-async fn main() -> eyre::Result<()> {
-    let cfg = config::get().expect("failed to read configuration");
+async fn main() {
+    let cfg = match config::get() {
+        Ok(cfg) => cfg,
+        Err(e) => {
+            eprintln!("failed to read configuration: {e}");
+            std::process::exit(2);
+        }
+    };
     let cfg_ser = serde_json::to_string(&cfg)
         .expect("the json serializer should never fail when serializing to a string");
     eprintln!("config:\n{cfg_ser}");
@@ -22,6 +27,4 @@ async fn main() -> eyre::Result<()> {
         .expect("failed creating composer")
         .run_until_stopped()
         .await;
-
-    Ok(())
 }
