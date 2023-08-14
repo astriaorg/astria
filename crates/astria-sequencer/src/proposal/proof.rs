1525a242c3f3bb824e794185094dd044190e112b
elizabeth <elizabethjbinks@gmail.com>
2023-08-14 16:48:45 -0400

    begin inclusion proofs w ct_merkle lib

--- crates/astria-sequencer/src/proposal/proof.rs
+++ crates/astria-sequencer/src/proposal/proof.rs
@@ -1,25 +1,61 @@
-use tendermint::merkle::MerkleHash;
+use ct_merkle::{
+    inclusion::InclusionProof as CtInclusionProof,
+    RootHash,
+};
+use sha2::Sha256;
 use tendermint::Hash;
-use digest::{consts::U32, Digest, FixedOutputReset};
 
-/// A merkle proof.
-pub(crate) struct Proof {
-    /// root hash of the merkle tree
-    root_hash: Hash,
-    /// leaf data before being hashed
-    leaf_data: Vec<u8>,
-    /// merkle proof
-    proof: Vec<Hash>,
+/// A merkle proof of inclusion.
+pub(crate) struct InclusionProof {
+    // value to be proven as included
+    value: Vec<u8>,
+    // leaf index of value to be proven
+    idx: usize,
+    // total number of leaves in the tree
+    num_leaves: usize,
+    // the merkle proof itself
+    inclusion_proof: CtInclusionProof<Sha256>,
 }
 
-impl Proof {
-    pub(crate) fn verify<H: MerkleHash + Default + Digest<OutputSize = U32> + FixedOutputReset>(&self) -> bool {
-        let mut hasher = H::default();
-        let mut hash = hasher.leaf_hash(&self.leaf_data);
+impl InclusionProof {
+    /// Verify that the proof is valid for the given root hash.
+    pub(crate) fn verify(&self, root_hash: Hash) -> anyhow::Result<()> {
+        let digest = sha2::digest::Output::<Sha256>::from_slice(root_hash.as_bytes()).to_owned();
+        let ct_root = RootHash::<Sha256>::new(digest, self.num_leaves);
+        ct_root
+            .verify_inclusion(&self.value, self.idx, &self.inclusion_proof)
+            .map_err(|e| anyhow::anyhow!("failed to verify inclusion: {}", e))
+    }
+}
 
-        for sibling in &self.proof {
-            // TOOD
-        }
-        hash == self.root_hash.as_bytes()
+#[cfg(test)]
+mod test {
+    use ct_merkle::{
+        self,
+        CtMerkleTree,
+    };
+    use tendermint::merkle::simple_hash_from_byte_vectors;
+
+    #[test]
+    fn ct_merkle_vs_tendermint() {
+        let data: Vec<Vec<u8>> = vec![
+            vec![1, 2, 3],
+            vec![4, 5, 6],
+            vec![7, 8, 9],
+            vec![10, 11, 12],
+            vec![13, 14, 15],
+            vec![16, 17, 18],
+            vec![19, 20, 21],
+            vec![22, 23, 24],
+        ];
+
+        let tm_root = simple_hash_from_byte_vectors::<tendermint::crypto::default::Sha256>(&data);
+
+        let mut ct_tree: CtMerkleTree<sha2::Sha256, Vec<u8>> = ct_merkle::CtMerkleTree::new();
+        data.iter().for_each(|d| {
+            ct_tree.push(d.to_vec());
+        });
+        let ct_root = ct_tree.root();
+        assert_eq!(ct_root.as_bytes().as_slice(), tm_root.as_slice());
     }
 }
