d5877d23d672eb2d913c8448fe27ffb9e17abdf5
Sam Bukowski <sam.c.bukowski@gmail.com>
2023-08-18 15:50:17 -0600

    update import for v1alpha1 dep

--- crates/astria-conductor/src/executor/queue.rs
+++ crates/astria-conductor/src/executor/queue.rs
@@ -246,7 +246,7 @@ mod test {
         sync::Arc,
     };
 
-    use astria_proto::execution::v1alpha1::{
+    use astria_proto::generated::execution::v1alpha1::{
         DoBlockResponse,
         InitStateResponse,
     };
