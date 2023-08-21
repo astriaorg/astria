4444591df0981a4e967dcfceaf20fecef8a44040
Richard Janis Goldschmidt <github@aberrat.io>
2023-08-21 16:59:24 +0200

    composer: ingest transactions from multiple rollups (#278)
    This is second PR of three PRs to support reading from multiple rollups
    in composer.
    
    This patch defines the `Collector` type as a long running tasks that
    read transactions from a different rollup each and submit them to the
    searcher together with a chain ID. Because the searcher has to keep
    track of the nonce, it still converts and signs the different
    transactions when converting them to sequencer blocks.
    
    This patch enables https://github.com/astriaorg/astria/issues/214
    
    - Define the `Collector` type, which reads new pending transactions from
    an EVM rollup supporting the ethereum execution API.
    - Replace the the new pending transaction stream in searcher by an mpsc
    channel that receives from a set of collector.
    - Spin up one collector per `<chain_id>::<url>` pair provided through
    config.
    - Each collector tags its transaction with its unique chain ID.
    
    The blackbox tests were extended to read from 2 different collectors
    implemented in terms of two separate mock servers.
    
    Sibling PRs:
    
    - https://github.com/astriaorg/astria/pull/277
    - https://github.com/astriaorg/astria/pull/278 (the present PR)
    - https://github.com/astriaorg/astria/pull/282
    
    - https://github.com/astriaorg/astria/issues/214 (will be closed after
    issue 277 is merged)

--- crates/astria-composer/src/searcher/mod.rs
+++ crates/astria-composer/src/searcher/mod.rs
@@ -14,19 +14,10 @@ use astria_sequencer::transaction;
 use color_eyre::eyre::{
     self,
     bail,
-    ensure,
     eyre,
     WrapErr as _,
 };
 use ed25519_consensus::SigningKey;
-use ethers::{
-    providers::{
-        Provider,
-        ProviderError,
-        Ws,
-    },
-    types::Transaction,
-};
 use humantime::format_duration;
 use secrecy::{
     ExposeSecret as _,
@@ -39,25 +30,30 @@ use sequencer_client::{
 use tendermint::abci;
 use tokio::{
     select,
-    sync::watch,
+    sync::{
+        mpsc::{
+            self,
+            Receiver,
+        },
+        watch,
+    },
     task::JoinSet,
 };
 use tracing::{
     debug,
-    info,
     instrument,
     warn,
 };
 
 use crate::Config;
 
+mod collector;
 mod rollup;
 
-/// `Searcher` receives transactions from an ethereum rollup, wraps them, and submits them to
+use collector::Collector;
+
 /// the astria seqeuencer.
 pub(super) struct Searcher {
-    // The client for getting new pending transactions from an ethereum rollup.
-    eth_client: EthClient,
     // The client for submitting wrapped and signed pending eth transactions to the astria
     // sequencer.
     sequencer_client: SequencerClient,
@@ -65,11 +61,13 @@ pub(super) struct Searcher {
     sequencer_key: SigningKey,
     // Nonce of the sequencer account we sign with
     sequencer_nonce: Arc<AtomicU32>,
-    // Chain ID to identify in the astria sequencer block which rollup a serialized sequencer
-    // action belongs to.
-    rollup_chain_id: String,
     // Channel to report the internal status of the searcher to other parts of the system.
     status: watch::Sender<Status>,
+    collectors: HashMap<String, Collector>,
+    collector_statuses: HashMap<String, watch::Receiver<collector::Status>>,
+    // A channel on which the searcher receives transactions from its collectors.
+    new_transactions: Receiver<collector::Transaction>,
+    collector_tasks: tokio_util::task::JoinMap<String, eyre::Result<()>>,
     // Set of currently running jobs converting pending eth transactions to signed sequencer
     // transactions.
     conversion_tasks: JoinSet<eyre::Result<transaction::Signed>>,
@@ -79,41 +77,13 @@ pub(super) struct Searcher {
 
 #[derive(Debug, Default)]
 pub(crate) struct Status {
-    eth_connected: bool,
+    all_collectors_connected: bool,
     sequencer_connected: bool,
 }
 
 impl Status {
     pub(crate) fn is_ready(&self) -> bool {
-        self.eth_connected && self.sequencer_connected
-    }
-}
-
-/// A thin wrapper around [`Provider<Ws>`] to add timeouts.
-///
-/// Currently only provides a timeout around for `get_net_version`.
-/// TODO(https://github.com/astriaorg/astria/issues/216): add timeouts for
-/// `subscribe_full_pendings_txs` (more complex because it's a stream).
-#[derive(Clone)]
-struct EthClient {
-    inner: Provider<Ws>,
-}
-
-impl EthClient {
-    async fn connect(url: &str) -> Result<Self, ProviderError> {
-        let inner = Provider::connect(url).await?;
-        Ok(Self {
-            inner,
-        })
-    }
-
-    /// Wrapper around [`Provider::get_net_version`] with a 1s timeout.
-    async fn get_net_version(&self) -> eyre::Result<String> {
-        use ethers::providers::Middleware as _;
-        tokio::time::timeout(Duration::from_secs(1), self.inner.get_net_version())
-            .await
-            .wrap_err("request timed out")?
-            .wrap_err("RPC returned with error")
+        self.all_collectors_connected && self.sequencer_connected
     }
 }
 
@@ -154,24 +124,53 @@ impl Searcher {
     /// + failed to connect to the eth RPC server;
     /// + failed to construct a sequencer clinet
     pub(super) async fn from_config(cfg: &Config) -> eyre::Result<Self> {
+        use futures::{
+            FutureExt as _,
+            StreamExt as _,
+        };
         use rollup::Rollup;
-        let mut rollups = cfg
+        let rollups = cfg
             .rollups
             .split(',')
             .filter(|s| !s.is_empty())
             .map(|s| Rollup::parse(s).map(Rollup::into_parts))
             .collect::<Result<HashMap<_, _>, _>>()
             .wrap_err("failed parsing provided <chain_id>::<url> pairs as rollups")?;
-        ensure!(
-            rollups.len() == 1,
-            "currently only 1 rollup is supported; got {}",
-            rollups.len(),
-        );
-        // connect to eth node
-        let (rollup_chain_id, execution_url) = rollups.drain().next().unwrap();
-        let eth_client = EthClient::connect(&execution_url)
-            .await
-            .wrap_err("failed connecting to eth")?;
+
+        let (tx_sender, new_transactions) = mpsc::channel(256);
+
+        let mut create_collectors = rollups
+            .into_iter()
+            .map(|(chain_id, url)| {
+                let task_name = chain_id.clone();
+                tokio::spawn(Collector::new(chain_id, url, tx_sender.clone()))
+                    .map(move |result| (task_name, result))
+            })
+            .collect::<futures::stream::FuturesUnordered<_>>();
+        // TODO(https://github.com/astriaorg/astria/issues/287): add timeouts or abort handles
+        // so this doesn't stall the entire server from coming up.
+        let mut collectors = HashMap::new();
+        while let Some((chain_id, join_result)) = create_collectors.next().await {
+            match join_result {
+                Err(err) => {
+                    return Err(err).wrap_err_with(|| {
+                        format!("task starting collector for {chain_id} panicked")
+                    });
+                }
+                Ok(Err(err)) => {
+                    return Err(err)
+                        .wrap_err_with(|| format!("failed starting collector for {chain_id}"));
+                }
+                Ok(Ok(collector)) => {
+                    collectors.insert(chain_id, collector);
+                }
+            }
+        }
+
+        let collector_statuses = collectors
+            .iter()
+            .map(|(chain_id, collector)| (chain_id.clone(), collector.subscribe()))
+            .collect();
 
         // connect to sequencer node
         let sequencer_client = SequencerClient::new(&cfg.sequencer_url)
@@ -189,12 +188,14 @@ impl Searcher {
         private_key_bytes.zeroize();
 
         Ok(Searcher {
-            eth_client,
             sequencer_client,
             sequencer_key,
             sequencer_nonce: Arc::new(AtomicU32::new(0)),
-            rollup_chain_id,
             status,
+            collectors,
+            collector_statuses,
+            new_transactions,
+            collector_tasks: tokio_util::task::JoinMap::new(),
             conversion_tasks: JoinSet::new(),
             submission_tasks: JoinSet::new(),
         })
@@ -205,14 +206,17 @@ impl Searcher {
     }
 
     /// Serializes and signs a sequencer tx from a rollup tx.
-    fn handle_pending_tx(&mut self, rollup_tx: Transaction) {
+    fn handle_pending_tx(&mut self, tx: collector::Transaction) {
         use astria_sequencer::{
             accounts::types::Nonce,
-            sequence,
             transaction::action,
         };
 
-        let chain_id = self.rollup_chain_id.clone();
+        let collector::Transaction {
+            chain_id,
+            inner: rollup_tx,
+        } = tx;
+
         let sequencer_key = self.sequencer_key.clone();
         let nonce = self.sequencer_nonce.clone();
 
@@ -220,7 +224,7 @@ impl Searcher {
             // Pack into sequencer tx
             let data = rollup_tx.rlp().to_vec();
             let chain_id = chain_id.into_bytes();
-            let seq_action = action::Action::SequenceAction(sequence::Action::new(chain_id, data));
+            let seq_action = action::Action::new_sequence_action(chain_id, data);
 
             // get current nonce and increment nonce
             let curr_nonce = nonce.fetch_add(1, Ordering::Relaxed);
@@ -255,17 +259,13 @@ impl Searcher {
 
     /// Runs the Searcher
     pub(super) async fn run(mut self) -> eyre::Result<()> {
-        use ethers::providers::{
-            Middleware as _,
-            StreamExt as _,
-        };
-        let wait_for_eth = self.wait_for_eth(5, Duration::from_secs(5), 2.0);
+        self.spawn_collectors();
+        let wait_for_collectors = self.wait_for_collectors();
         let wait_for_seq = self.wait_for_sequencer(5, Duration::from_secs(5), 2.0);
-        match tokio::try_join!(wait_for_eth, wait_for_seq) {
+        match tokio::try_join!(wait_for_collectors, wait_for_seq) {
             Ok(((), ())) => {}
             Err(err) => return Err(err).wrap_err("failed to start searcher"),
         }
-
         // set initial sequencer nonce
         let address = Address::from_verification_key(self.sequencer_key.verification_key());
         let nonce_response = self
@@ -277,16 +277,10 @@ impl Searcher {
         self.sequencer_nonce
             .store(nonce_response.nonce, Ordering::Relaxed);
 
-        let eth_client = self.eth_client.inner.clone();
-        let mut tx_stream = eth_client
-            .subscribe_full_pending_txs()
-            .await
-            .wrap_err("failed to subscribe eth client to full pending transactions")?;
-
         loop {
             select!(
                 // serialize and sign sequencer tx for incoming pending rollup txs
-                Some(rollup_tx) = tx_stream.next() => self.handle_pending_tx(rollup_tx),
+                Some(rollup_tx) = self.new_transactions.recv() => self.handle_pending_tx(rollup_tx),
 
                 // submit signed sequencer txs to sequencer
                 Some(join_result) = self.conversion_tasks.join_next(), if !self.conversion_tasks.is_empty() => {
@@ -306,7 +300,8 @@ impl Searcher {
                     match join_result {
                         Ok(Ok(())) => {}
                         Ok(Err(e)) =>
-                            // TODO: Decide what to do if submitting to sequencer failed. Should it be resubmitted?
+                            // TODO(https://github.com/astriaorg/astria/issues/246): What to do if
+                            // submitting fails. Resubmit?
                             warn!(error.message = %e, error.cause_chain = ?e, "failed to submit signed sequencer transaction to sequencer"),
                         Err(e) => warn!(
                             error.message = %e,
@@ -323,51 +318,54 @@ impl Searcher {
         Ok(())
     }
 
-    /// Wait until a connection to eth is established.
-    ///
-    /// This function tries to establish a connection to eth by
-    /// querying its net_version RPC. If it fails, it retries for another `n_retries`
-    /// times with exponential backoff.
-    ///
-    /// # Errors
-    ///
-    /// An error is returned if calling eth failed after `n_retries + 1` times.
-    #[instrument(skip_all, fields(
-        retries.max_number = n_retries,
-        retries.initial_delay = %format_duration(delay),
-        retries.exponential_factor = factor,
-    ))]
-    async fn wait_for_eth(
-        &self,
-        n_retries: usize,
-        delay: Duration,
-        factor: f32,
-    ) -> eyre::Result<()> {
-        use backon::{
-            ExponentialBuilder,
-            Retryable as _,
+    /// Spawns all collector on the collector task set.
+    fn spawn_collectors(&mut self) {
+        for (chain_id, collector) in self.collectors.drain() {
+            self.collector_tasks
+                .spawn(chain_id, collector.run_until_stopped());
+        }
+    }
+
+    /// Waits for all collectors to come online.
+    async fn wait_for_collectors(&self) -> eyre::Result<()> {
+        use futures::{
+            future::FutureExt as _,
+            stream::{
+                FuturesUnordered,
+                StreamExt as _,
+            },
         };
-        debug!("attempting to connect to eth");
-        let backoff = ExponentialBuilder::default()
-            .with_min_delay(delay)
-            .with_factor(factor)
-            .with_max_times(n_retries);
-        let version = (|| {
-            let client = self.eth_client.clone();
-            // This is using `get_net_version` because that's what ethers' Middleware is
-            // implementing. Maybe the `net_listening` RPC would be better, but ethers
-            // does not have that.
-            async move { client.get_net_version().await }
-        })
-        .retry(&backoff)
-        .notify(|err, dur| warn!(error.msg = %err, retry_in = %format_duration(dur), "failed issuing RPC; retrying"))
-        .await
-        .wrap_err(
-            "failed to retrieve net version from eth after seferal retries",
-        )?;
-        info!(version, rpc = "net_version", "RPC was successful");
+        let mut statuses = self
+            .collector_statuses
+            .iter()
+            .map(|(chain_id, status)| {
+                let mut status = status.clone();
+                async move {
+                    match status.wait_for(collector::Status::is_connected).await {
+                        // `wait_for` returns a reference to status; throw it
+                        // away because this future cannot return a reference to
+                        // a stack local object.
+                        Ok(_) => Ok(()),
+                        // if an collector fails while waiting for its status, this
+                        // will return an error
+                        Err(e) => Err(e),
+                    }
+                }
+                .map(|fut| (chain_id.clone(), fut))
+            })
+            .collect::<FuturesUnordered<_>>();
+        while let Some((chain_id, maybe_err)) = statuses.next().await {
+            if let Err(e) = maybe_err {
+                return Err(e).wrap_err_with(|| {
+                    format!(
+                        "collector for chain ID {chain_id} failed while waiting for it to become \
+                         ready"
+                    )
+                });
+            }
+        }
         self.status.send_modify(|status| {
-            status.eth_connected = true;
+            status.all_collectors_connected = true;
         });
         Ok(())
     }
