9d75fb55f8016711306a551bf72d38bed873d7ee
Richard Janis Goldschmidt <github@aberrat.io>
2023-09-20 14:47:21 +0200

    conductor: replace gossipnet by a subscription to cometbft/sequencer (#383)
    ## Summary
    This patch replaces gossipnet by a websocket stream of new blocks
    directly from sequencer.
    
    ## Background
    
    Adding a p2p layer adds a lot of complexity to astria services. With
    this patch conductor reads new blocks directly off of
    cometbft/sequencer, making it much simpler. For background reading, see
    this issue: https://github.com/astriaorg/astria/issues/325
    
    ## Changes
    - Replace gossipnet directly by a stream of new blocks using the
    cometbft subscrition RPC.
    - No other changes in regard to how blocks are further processed has
    been made.
    
    ## Testing
    We are relying on the rust tendermint-rpc crate functioning properly. As
    a follow-up it makes sense to create blackbox tests for conductor, which
    are currently absent.
    
    ## Breaking Changelist
    - Reading over p2p is no longer supported. Deployments must be updated
    and wired to read off of cometbft/sequenceer.
    
    ## Related Issues
    Precursor PRs unblocking this work:
    https://github.com/astriaorg/astria/pull/375
    https://github.com/astriaorg/astria/pull/376
    https://github.com/astriaorg/astria/pull/382
    
    closes https://github.com/astriaorg/astria/issues/332
    
    ---------
    
    Co-authored-by: Emilia Hane <emiliaha95@gmail.com>

--- crates/astria-conductor/src/lib.rs
+++ crates/astria-conductor/src/lib.rs
@@ -2,18 +2,17 @@
 //! The Astria conductor connects the shared sequencer layer and the execution layer.
 //! When a block is received from the sequencer layer, the conductor pushes it to the execution
 //! layer. There are two ways for a block to be received:
-//! - via the gossip network
+//! - pushed from the shared-sequencer
 //! - via the data availability layer
 //! In the first case, the block is pushed to the execution layer, executed, and added to the
-//! blockchain. It's marked as a soft commitment; the block is not finalized until it's received
-//! from the data availability layer. In the second case, the execution layer is notified to mark
-//! the block as finalized.
+//! blockchain. It's marked as a soft commitment; the block is not regarded as finalized on the
+//! execution layer until it's received from the data availability layer. In the second case, the
+//! execution layer is notified to mark the block as finalized.
 pub mod block_verifier;
 pub mod config;
 pub mod driver;
 pub(crate) mod execution_client;
 pub(crate) mod executor;
-pub mod network;
 pub mod reader;
 pub(crate) mod types;
 
