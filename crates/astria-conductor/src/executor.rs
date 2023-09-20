822fcbf397c249e2104a0ae16a6508993ecca76f
Jesse Snyder <jessetsnyder@gmail.com>
2023-09-20 12:02:43 -0500

    use deref coercion instead of clone

--- crates/astria-conductor/src/executor.rs
+++ crates/astria-conductor/src/executor.rs
@@ -227,7 +227,7 @@ impl<C: ExecutionClient> Executor<C> {
         {
             debug!(
                 height = block.header.height.value(),
-                execution_hash = hex::encode(execution_block.hash.clone()),
+                execution_hash = hex::encode(&execution_block.hash),
                 "block already executed"
             );
             return Ok(Some(execution_block.clone()));
