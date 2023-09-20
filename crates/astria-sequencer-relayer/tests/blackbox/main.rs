69830091c971ee90f221663212e3058664ae37f8
Richard Janis Goldschmidt <github@aberrat.io>
2023-09-20 15:48:12 +0200

    move the timeout-advance-timeout block to a helper fn

--- crates/astria-sequencer-relayer/tests/blackbox/main.rs
+++ crates/astria-sequencer-relayer/tests/blackbox/main.rs
@@ -5,6 +5,7 @@ use std::time::Duration;
 use helper::{
     spawn_sequencer_relayer,
     CelestiaMode,
+    TestSequencerRelayer,
 };
 use tokio::{
     sync::mpsc::error::TryRecvError,
@@ -13,25 +14,32 @@ use tokio::{
         timeout,
     },
 };
+use wiremock::MockGuard;
+
+/// Small hack because sometimes test choreography doesn't work properly, with
+/// sequencer-relayer trying to get the latest block too early or too late.
+/// This ensures that 1. the guard times out (so the test does not run indefinitely),
+/// but that 2. sequencer-relayer's timer is triggered, pulling a new block from the mock.
+async fn timeout_guard(test_env: &TestSequencerRelayer, guard: MockGuard) {
+    if timeout(Duration::from_millis(100), guard.wait_until_satisfied())
+        .await
+        .is_ok()
+    {
+        return;
+    }
+    time::pause();
+    test_env.advance_by_block_time().await;
+    time::resume();
+    timeout(Duration::from_millis(100), guard.wait_until_satisfied())
+        .await
+        .unwrap();
+}
 
 #[tokio::test(flavor = "current_thread")]
 async fn one_block_is_relayed_to_celestia() {
     let mut sequencer_relayer = spawn_sequencer_relayer(CelestiaMode::Immediate).await;
-    'first_latest_block: {
-        let guard = sequencer_relayer.mount_block_response(1).await;
-        if timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .is_ok()
-        {
-            break 'first_latest_block;
-        }
-        time::pause();
-        sequencer_relayer.advance_by_block_time().await;
-        time::resume();
-        timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .unwrap();
-    }
+    let guard = sequencer_relayer.mount_block_response(1).await;
+    timeout_guard(&sequencer_relayer, guard).await;
 
     let Some(blobs_seen_by_celestia) = sequencer_relayer
         .celestia
@@ -52,21 +60,8 @@ async fn one_block_is_relayed_to_celestia() {
 #[tokio::test(flavor = "current_thread")]
 async fn same_block_is_dropped() {
     let mut sequencer_relayer = spawn_sequencer_relayer(CelestiaMode::Immediate).await;
-    'latest_block: {
-        let guard = sequencer_relayer.mount_block_response(1).await;
-        if timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .is_ok()
-        {
-            break 'latest_block;
-        }
-        time::pause();
-        sequencer_relayer.advance_by_block_time().await;
-        time::resume();
-        timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .unwrap();
-    }
+    let guard = sequencer_relayer.mount_block_response(1).await;
+    timeout_guard(&sequencer_relayer, guard).await;
 
     // The first block should be received immediately
     let Some(blobs_seen_by_celestia) = sequencer_relayer
@@ -80,21 +75,8 @@ async fn same_block_is_dropped() {
     assert_eq!(blobs_seen_by_celestia.len(), 2);
 
     // Mount the same block again and advance by the block time to ensure its picked up.
-    'latest_block: {
-        let guard = sequencer_relayer.mount_block_response(1).await;
-        if timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .is_ok()
-        {
-            break 'latest_block;
-        }
-        time::pause();
-        sequencer_relayer.advance_by_block_time().await;
-        time::resume();
-        timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .unwrap();
-    }
+    let guard = sequencer_relayer.mount_block_response(1).await;
+    timeout_guard(&sequencer_relayer, guard).await;
 
     match sequencer_relayer.celestia.state_rpc_confirmed_rx.try_recv() {
         Err(TryRecvError::Empty) => {}
@@ -108,69 +90,17 @@ async fn slow_celestia_leads_to_bundled_blobs() {
     // (it takes 4000 ms to respond if the sequencer block time is 1000 ms).
     let mut sequencer_relayer = spawn_sequencer_relayer(CelestiaMode::Delayed(4)).await;
 
-    'latest_block: {
-        let guard = sequencer_relayer.mount_block_response(1).await;
-        if timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .is_ok()
-        {
-            break 'latest_block;
-        }
-        time::pause();
-        sequencer_relayer.advance_by_block_time().await;
-        time::resume();
-        timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .unwrap();
-    }
+    let guard = sequencer_relayer.mount_block_response(1).await;
+    timeout_guard(&sequencer_relayer, guard).await;
 
-    'latest_block: {
-        let guard = sequencer_relayer.mount_block_response(2).await;
-        if timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .is_ok()
-        {
-            break 'latest_block;
-        }
-        time::pause();
-        sequencer_relayer.advance_by_block_time().await;
-        time::resume();
-        timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .unwrap();
-    }
+    let guard = sequencer_relayer.mount_block_response(2).await;
+    timeout_guard(&sequencer_relayer, guard).await;
 
-    'latest_block: {
-        let guard = sequencer_relayer.mount_block_response(3).await;
-        if timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .is_ok()
-        {
-            break 'latest_block;
-        }
-        time::pause();
-        sequencer_relayer.advance_by_block_time().await;
-        time::resume();
-        timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .unwrap();
-    }
+    let guard = sequencer_relayer.mount_block_response(3).await;
+    timeout_guard(&sequencer_relayer, guard).await;
 
-    'latest_block: {
-        let guard = sequencer_relayer.mount_block_response(4).await;
-        if timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .is_ok()
-        {
-            break 'latest_block;
-        }
-        time::pause();
-        sequencer_relayer.advance_by_block_time().await;
-        time::resume();
-        timeout(Duration::from_millis(100), guard.wait_until_satisfied())
-            .await
-            .unwrap();
-    }
+    let guard = sequencer_relayer.mount_block_response(4).await;
+    timeout_guard(&sequencer_relayer, guard).await;
 
     // But celestia sees a pair of blobs (1 block + sequencer namespace data)
     if let Some(blobs_seen_by_celestia) = sequencer_relayer
@@ -191,6 +121,4 @@ async fn slow_celestia_leads_to_bundled_blobs() {
     {
         assert_eq!(6, blobs_seen_by_celestia.len());
     }
-
-    // TODO: we should shut down and join all outstanding tasks here.
 }
