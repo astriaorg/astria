f5443eabef21e0069a29d120db95865c965d1e6e
elizabeth <elizabethjbinks@gmail.com>
2023-09-21 16:20:51 -0400

    fix unwrap

--- crates/astria-sequencer/src/service/mempool.rs
+++ crates/astria-sequencer/src/service/mempool.rs
@@ -74,7 +74,18 @@ fn handle_check_tx(req: request::CheckTx) -> response::CheckTx {
             };
         }
     };
-    let signed_tx = SignedTransaction::try_from_raw(raw_signed_tx).unwrap();
+    let signed_tx = match SignedTransaction::try_from_raw(raw_signed_tx) {
+        Ok(tx) => tx,
+        Err(e) => {
+            return response::CheckTx {
+                code: AbciCode::INVALID_SIGNATURE.into(),
+                info: "failed to verify signed transaction".into(),
+                log: format!("{e:?}"),
+                ..response::CheckTx::default()
+            };
+        }
+    };
+
     // if the tx passes the check, status code 0 is returned.
     // TODO(https://github.com/astriaorg/astria/issues/228): status codes for various errors
     // TODO(https://github.com/astriaorg/astria/issues/319): offload `check_stateless` using `deliver_tx_bytes` mechanism
--- crates/astria-sequencer/src/service/mod.rs
+++ crates/astria-sequencer/src/service/mod.rs
@@ -19,6 +19,7 @@ impl AbciCode {
     pub(crate) const INVALID_PARAMETER: Self = Self(2);
     pub(crate) const INTERNAL_ERROR: Self = Self(3);
     pub(crate) const INVALID_NONCE: Self = Self(4);
+    pub(crate) const INVALID_SIGNATURE: Self = Self(5);
 }
 
 impl AbciCode {
@@ -29,6 +30,7 @@ impl AbciCode {
             2 => Some("one or more path parameters were invalid"),
             3 => Some("an internal server error occured"),
             4 => Some("the provided nonce was invalid"),
+            5 => Some("the provided signature was invalid"),
             _ => None,
         }
     }
