646ad0979f8c516c15f1a4d0e1ec128a084ce696
Richard Janis Goldschmidt <github@aberrat.io>
2023-09-26 15:25:08 +0200

    sequencer-validation: hash chain id before creating merkle tree
    also exports a sha256_hash utility

--- crates/astria-sequencer-validation/src/utils.rs
+++ crates/astria-sequencer-validation/src/utils.rs
@@ -11,9 +11,16 @@ pub fn generate_action_tree_leaves<T: AsRef<[u8]>>(
     let mut leaves = Vec::new();
     for (chain_id, txs) in chain_id_to_txs {
         let root = MerkleTree::from_leaves(txs).root();
-        let mut leaf = chain_id.as_ref().to_vec();
+        let mut leaf = sha256_hash(chain_id.as_ref()).to_vec();
         leaf.append(&mut root.to_vec());
         leaves.push(leaf);
     }
     leaves
+}
+
+pub fn sha256_hash(data: &[u8]) -> [u8; 32] {
+    use sha2::Digest as _;
+    let mut hasher = sha2::Sha256::new();
+    hasher.update(data);
+    hasher.finalize().into()
 }
