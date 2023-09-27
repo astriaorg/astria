fee39b0b3f1d518799f9f5960acd3d88e68f70ab
Jordan Oroshiba <jordan.oroshiba@gmail.com>
2023-09-27 16:49:12 -0700

    force test

--- crates/astria-composer/tests/blackbox/api.rs
+++ crates/astria-composer/tests/blackbox/api.rs
@@ -11,6 +11,6 @@ async fn readyz_with_one_rollup() {
 async fn readyz_with_two_rollups() {
     // spawn_composer hits `/readyz` as part of starting the test
     // environment. If this future return then `readyz` must have
-    // returned `status: ok`.
+    // returned `status: ok`. blah
     let _test_composer = spawn_composer(&["test1", "test2"]).await;
 }
