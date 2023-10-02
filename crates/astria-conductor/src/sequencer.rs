438171fa53f8ddee36e48a40418bb9ec33ec6483
Richard Janis Goldschmidt <github@aberrat.io>
2023-10-02 09:40:19 +0200

    attempt to resubscribe to new blocks after failure

--- crates/astria-conductor/src/sequencer.rs
+++ crates/astria-conductor/src/sequencer.rs
@@ -6,7 +6,10 @@ use color_eyre::eyre::{
     self,
     WrapErr as _,
 };
-use sequencer_client::NewBlockStreamError;
+use sequencer_client::{
+    extension_trait::NewBlocksStream,
+    NewBlockStreamError,
+};
 use tokio::{
     select,
     sync::oneshot,
@@ -48,18 +51,18 @@ impl Reader {
     /// Run the sequencer reader event loop
     #[instrument(skip_all)]
     pub(crate) async fn run_until_stopped(mut self) -> eyre::Result<()> {
-        use futures::StreamExt as _;
-        use sequencer_client::SequencerSubscriptionClientExt as _;
+        use futures::{
+            FutureExt as _,
+            StreamExt as _,
+        };
 
-        info!("Starting driver event loop.");
-        let mut new_blocks = self
-            .pool
-            .get()
-            .await
-            .wrap_err("failed getting an initial sequencer client from the pool")?
-            .subscribe_new_block_data()
-            .await
-            .wrap_err("failed subscribing to sequencer to receive new blocks")?;
+        let mut new_blocks: futures::stream::Fuse<NewBlocksStream> =
+            subscribe_new_blocks(self.pool.clone())
+                .await
+                .wrap_err("failed to start initial new-blocks subscription")?
+                .fuse();
+
+        let mut resubscribe = futures::future::Fuse::terminated();
         loop {
             select! {
                 shutdown = &mut self.shutdown => {
@@ -70,12 +73,29 @@ impl Reader {
                     break;
                 }
 
+                res = &mut resubscribe => {
+                    match res {
+                        Ok(Ok::<NewBlocksStream, _>(subscription)) => {
+                            new_blocks = subscription.fuse();
+                        }
+                        Ok(Err(e)) => {
+                            warn!(error.message = %e, error.cause = ?e, "failed subscribing to new blocks from sequencer; exiting reader");
+                            break;
+                        }
+                        Err(e) => {
+                            warn!(error.message = %e, error.cause = ?e, "task attempting to subscribe to new blocks from sequencer failed; exiting reader");
+                            break;
+                        }
+                    }
+                }
+
                 new_block = new_blocks.next() => {
                     if let Some(block) = new_block {
                         self.handle_new_block(block)
                     } else {
-                        warn!("sequencer new-block subscription closed unexpectedly; shutting down driver");
-                        break;
+                        warn!("sequencer new-block subscription closed unexpectedly; attempting to resubscribe");
+                        let pool = self.pool.clone();
+                        resubscribe = tokio::spawn(subscribe_new_blocks(pool)).fuse();
                     }
                 }
             }
@@ -101,4 +121,16 @@ impl Reader {
             warn!(err.msg = %err, err.cause = ?err, "failed sending new block received from sequencer to executor");
         }
     }
+}
+
+async fn subscribe_new_blocks(
+    pool: deadpool::managed::Pool<crate::client_provider::ClientProvider>,
+) -> eyre::Result<NewBlocksStream> {
+    use sequencer_client::SequencerSubscriptionClientExt as _;
+    pool.get()
+        .await
+        .wrap_err("failed getting a sequencer client from the pool")?
+        .subscribe_new_block_data()
+        .await
+        .wrap_err("failed subscribing to sequencer to receive new blocks")
 }
