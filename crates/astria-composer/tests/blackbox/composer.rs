06f976043a40a24bf76f50d28fee536d7ce063dc
dereksione <derek@astria.org>
2023-10-05 12:02:40 -0400

    wip: tx integrity check

--- crates/astria-composer/tests/blackbox/composer.rs
+++ crates/astria-composer/tests/blackbox/composer.rs
@@ -55,6 +55,47 @@ async fn tx_from_two_rollups_are_received_by_sequencer() {
         .expect("mocked sequencer should have received a broadcast message from composer");
 }
 
+#[tokio::test]
+async fn tx_integrity_maintained() {
+    let test_composer = spawn_composer(&["test1", "test2"]).await;
+    let mock_guard = mount_broadcast_tx_sync_mock(&test_composer.sequencer, "test1", 42).await;
+
+    const TX_STR: &str = r#"{
+        "hash": "0x077daf1a23be6c48bf5e101b85cc79d9e81969ef901a7099b4fedac3c0d59809",
+        "nonce": "0x22e",
+        "blockHash": "0xae541fc4dc35d1d8bc2a018160e5ac8876d51ad76539d0b134ac5b82d64e7bda",
+        "blockNumber": "0x10fa231",
+        "transactionIndex": "0x1",
+        "from": "0xe398c02cf1e030b541bdc87efece27ad5ef1e783",
+        "to": "0x7a250d5630b4cf539739df2c5dacb4c659f2488d",
+        "value": "0x0",
+        "gasPrice": "0xb2703a824",
+        "gas": "0x7a120",
+        "input": "0x791ac94700000000000000000000000000000000000000000000000000000a29e1e7c600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000e398c02cf1e030b541bdc87efece27ad5ef1e7830000000000000000000000000000000000000000000000000000000064c5999f00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000ea778a02ab20ce0a8132a0e5312b53a5f23cef5000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+        "v": "0x0",
+        "r": "0xd768f4d808fc1cb0eedca99363b78d9fa42555b4f26cbf5fa48ba8af96bff159",
+        "s": "0x7f4cd55d6d06422ce14f58e72b0f366b479f606d129e4fc959a5eb348c93e888",
+        "type": "0x2",
+        "accessList": [],
+        "maxPriorityFeePerGas": "0x55ae82600",
+        "maxFeePerGas": "0x174876e800",
+        "chainId": "0x1"
+    }"#;
+
+    let tx: Transaction = serde_json::from_str(TX_STR).unwrap();
+
+    test_composer.rollup_nodes["test1"].push_tx(tx).unwrap();
+
+    tokio::time::timeout(
+        Duration::from_millis(100),
+        mock_guard.wait_until_satisfied(),
+    )
+    .await
+    .expect("mocked sequencer should have received a broadcast message from composer");
+
+}
+
+
 /// Deserizalizes the bytes contained in a `tx_sync::Request` to a
 /// signed sequencer transaction and verifies that the contained
 /// sequence action is for the given `expected_chain_id`.
@@ -68,6 +109,9 @@ async fn mount_broadcast_tx_sync_mock(
         native::sequencer::v1alpha1::SignedTransaction,
         Message as _,
     };
+
+    let tx_data;
+
     let matcher = move |request: &Request| {
         let wrapped_tx_sync_req: request::Wrapper<tx_sync::Request> =
             serde_json::from_slice(&request.body)
@@ -83,6 +127,7 @@ async fn mount_broadcast_tx_sync_mock(
         let Some(sequence_action) = sent_action.as_sequence() else {
             panic!("mocked sequencer expected a sequence action");
         };
+        tx_data = sequence_action.data;
         sequence_action.chain_id == expected_chain_id.as_bytes()
             && signed_tx.unsigned_transaction().nonce == expected_nonce
     };
@@ -90,7 +135,7 @@ async fn mount_broadcast_tx_sync_mock(
         Id::Num(1),
         Some(tx_sync::Response {
             code: 0.into(),
-            data: vec![].into(),
+            data: tx_data.into(),
             log: String::new(),
             hash: tendermint::Hash::Sha256([0; 32]),
         }),
