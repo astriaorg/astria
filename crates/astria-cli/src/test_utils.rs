751bb1c15c2dc0a0c7e04435f80b50e1aa2c7652
Jesse Snyder <jessetsnyder@gmail.com>
2023-10-10 22:18:49 -0600

    ignore poison

--- crates/astria-cli/src/test_utils.rs
+++ crates/astria-cli/src/test_utils.rs
@@ -1,6 +1,9 @@
 use std::{
     env,
-    sync::Mutex,
+    sync::{
+        Mutex,
+        PoisonError,
+    },
 };
 
 use once_cell::sync::Lazy;
@@ -9,29 +12,21 @@ use tempfile::TempDir;
 static CURRENT_DIR_LOCK: Lazy<Mutex<()>> = Lazy::new(|| Mutex::new(()));
 
 /// Run a closure with a temporary directory as the current directory.
-/// This is useful for tests that want to change the current directory.
-/// `set_current_env` is not thread safe, so it will cause flaky tests if not behind a mutex.
+/// This is useful for cleaning up after tests that test code that creates files.
+///
+/// A mutex is required because `set_current_env` is not thread safe, which
+/// causes flaky tests when run in parallel and it's called in multiple tests.
 pub(crate) fn with_temp_directory<F>(closure: F)
 where
     F: FnOnce(&TempDir),
 {
-    // Lock the mutex
-    let _guard = CURRENT_DIR_LOCK.lock().unwrap();
+    // ignore poisoning
+    let _guard = CURRENT_DIR_LOCK
+        .lock()
+        .unwrap_or_else(PoisonError::into_inner);
 
-    // Store the original current directory
-    let original_dir = env::current_dir().unwrap();
-
-    // Create a new temporary directory
     let temp_dir = TempDir::new().unwrap();
-
-    // Change to the temporary directory
     env::set_current_dir(&temp_dir).unwrap();
-
-    // Run the closure, passing it a reference to the temp directory
     closure(&temp_dir);
-
-    // Restore the original current directory
-    env::set_current_dir(original_dir).unwrap();
-
     temp_dir.close().unwrap();
 }
