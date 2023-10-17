6f9ae77fd8ff9c817d7fba6e8599e669775dec2e
jesse snyder <jessetsnyder@gmail.com>
2023-10-16 19:32:19 -0600

    astria cli (#457)
    ## Summary
    Astria CLI allows users to deploy rollups, get account keys from the
    Sequencer, and other things.
    
    ## Background
    We needed a quick cli for self-serve rollups.
    
    ## Changes
    - adds `astria-cli` crate to workspace
    - `Cli` struct 
    - `commands` module
    - `main.rs` gets args from `Cli` and passes them to `commands::run`
    
    ## Testing
    - proper cli arg parsing and validation from flags w/ `assert_cmd`
    - proper cli arg parsing and validation from envars w/ `assert_cmd`
    - proper conversion of cli args to Rust application types
    - rollup subcommands using temp directory isolation
    - sequencer account subcommands
    - NOTE: I did not write tests for the subcommands that use
    `SequencerClientExt`. They are just very thin wrappers around the
    sequencer/tendermint http client functionality.
    - utility functions
    
    ## Related Issues
    closes https://github.com/astriaorg/astria/issues/496
    https://github.com/astriaorg/astria/issues/497
    
    ---------
    
    Co-authored-by: Richard Janis Goldschmidt <github@aberrat.io>

---
+++ crates/astria-cli/tests/integration_test.rs
@@ -0,0 +1,58 @@
+use std::path::PathBuf;
+
+use assert_cmd::Command;
+use test_utils::with_temp_directory;
+
+#[test]
+fn test_envars_are_parsed_for_config_create() {
+    with_temp_directory(|_dir| {
+        let mut cmd = Command::cargo_bin("astria-cli").unwrap();
+
+        cmd.arg("rollup")
+            .arg("config")
+            .arg("create")
+            .env("ROLLUP_USE_TTY", "true")
+            .env("ROLLUP_LOG_LEVEL", "debug")
+            .env("ROLLUP_NAME", "testtest")
+            .env("ROLLUP_CHAIN_ID", "test_chain_id")
+            .env("ROLLUP_NETWORK_ID", "53")
+            .env("ROLLUP_SKIP_EMPTY_BLOCKS", "true")
+            .env(
+                "ROLLUP_GENESIS_ACCOUNTS",
+                "0xaC21B97d35Bf75A7dAb16f35b111a50e78A72F30:1000",
+            )
+            .env("ROLLUP_SEQUENCER_INITIAL_BLOCK_HEIGHT", "10")
+            .env("ROLLUP_SEQUENCER_WEBSOCKET", "ws://localhost:8080")
+            .env("ROLLUP_SEQUENCER_RPC", "http://localhost:8081")
+            .assert()
+            .success();
+
+        assert!(PathBuf::from("testtest-rollup-conf.yaml").exists());
+    });
+}
+
+#[test]
+fn test_error_when_incorrect_envar_values() {
+    with_temp_directory(|_dir| {
+        let mut cmd = Command::cargo_bin("astria-cli").unwrap();
+
+        cmd.arg("rollup")
+            .arg("config")
+            .arg("create")
+            .env("ROLLUP_USE_TTY", "not_a_bool")
+            .env("ROLLUP_LOG_LEVEL", "debug")
+            .env("ROLLUP_NAME", "testtest")
+            .env("ROLLUP_CHAIN_ID", "test_chain_id")
+            .env("ROLLUP_NETWORK_ID", "not_a_number")
+            .env("ROLLUP_SKIP_EMPTY_BLOCKS", "true")
+            .env(
+                "ROLLUP_GENESIS_ACCOUNTS",
+                "0xaC21B97d35Bf75A7dAb16f35b111a50e78A72F30:1000",
+            )
+            .env("ROLLUP_SEQUENCER_INITIAL_BLOCK_HEIGHT", "10")
+            .env("ROLLUP_SEQUENCER_WEBSOCKET", "ws://localhost:8080")
+            .env("ROLLUP_SEQUENCER_RPC", "http://localhost:8081")
+            .assert()
+            .failure();
+    });
+}
