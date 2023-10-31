defc3bc56612e838cdb32ddf4243c3ef7c877243
elizabeth <elizabethjbinks@gmail.com>
2023-10-30 20:13:24 -0400

    attempt to fix ci by installing solc

--- crates/astria-composer/tests/blackbox/composer.rs
+++ crates/astria-composer/tests/blackbox/composer.rs
@@ -5,9 +5,7 @@ use proto::generated::sequencer::v1alpha1::NonceResponse;
 use sequencer_types::AbciCode;
 use tendermint_rpc::{
     endpoint::broadcast::tx_sync,
-    request::{
-        self,
-    },
+    request::{self,},
     response,
     Id,
 };
