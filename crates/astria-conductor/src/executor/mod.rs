4b88016eb01dc1277bf91b17c11e27426b11463d
Sam Bukowski <sam.c.bukowski@gmail.com>
2023-11-06 18:28:50 -0700

    reduced ordering match to single if

--- crates/astria-conductor/src/executor/mod.rs
+++ crates/astria-conductor/src/executor/mod.rs
@@ -229,27 +229,13 @@ impl Executor {
     /// execution block hash.
     #[instrument(skip(self), fields(sequencer_block_hash = ?block.block_hash, sequencer_block_height = block.header.height.value()))]
     async fn execute_block(&mut self, block: SequencerBlockSubset) -> Result<Block> {
-        match self
-            .executable_block_height
-            .cmp(&(block.header.height.value() as u32))
-        {
-            std::cmp::Ordering::Less => {
-                return Err(eyre!(
-                    "incoming sequencer block at height {} is above executable block height of \
-                     {}, skipping execution of future block",
-                    block.header.height.value(),
-                    self.executable_block_height
-                ));
-            }
-            std::cmp::Ordering::Greater => {
-                return Err(eyre!(
-                    "incoming sequencer block at height {} is below executable block height of \
-                     {}, skipping execution of stale block",
-                    block.header.height.value(),
-                    self.executable_block_height
-                ));
-            }
-            _ => {}
+        if self.executable_block_height as u64 != block.header.height.value() {
+            error!(
+                sequencer_block_height = block.header.height.value(),
+                executable_block_height = self.executable_block_height,
+                "block received out of order;"
+            );
+            return Err(eyre!("block received out of order"));
         }
 
         if let Some(execution_block) = self
