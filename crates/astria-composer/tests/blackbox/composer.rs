6b021a29a7bddffe055a21199257ac49da004c2a
itamar <itamar.reif@gmail.com>
2023-11-08 19:49:33 -0500

    integrity test but decoding fails due to from and gas_limit

--- crates/astria-composer/tests/blackbox/composer.rs
+++ crates/astria-composer/tests/blackbox/composer.rs
@@ -1,7 +1,14 @@
 use std::time::Duration;
 
-use ethers::types::Transaction;
+use ethers::{
+    types::Transaction,
+    utils::rlp::{
+        Decodable,
+        Rlp,
+    },
+};
 use proto::generated::sequencer::v1alpha1::NonceResponse;
+use sequencer_client::SignedTransaction;
 use sequencer_types::AbciCode;
 use tendermint_rpc::{
     endpoint::broadcast::tx_sync,
@@ -150,6 +157,48 @@ async fn invalid_nonce_failure_causes_tx_resubmission_under_different_nonce() {
     .expect("valid nonce guard failed");
 }
 
+#[tokio::test]
+async fn test_single_tx_integrity() {
+    let test_composer = spawn_composer(&["test1"]).await;
+
+    // blockhash, blocknumber and transactionindex are generated after execution - we can leave them
+    // out
+    let txs = r#"{
+        "hash": "0x077daf1a23be6c48bf5e101b85cc79d9e81969ef901a7099b4fedac3c0d59809",
+        "nonce": "0x22e",
+        "from": "0xe398c02cf1e030b541bdc87efece27ad5ef1e783",
+        "to": "0x7a250d5630b4cf539739df2c5dacb4c659f2488d",
+        "value": "0x0",
+        "gasPrice": "0xb2703a824",
+        "gas": "0x7a120",
+        "input": "0x791ac94700000000000000000000000000000000000000000000000000000a29e1e7c600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000e398c02cf1e030b541bdc87efece27ad5ef1e7830000000000000000000000000000000000000000000000000000000064c5999f00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000ea778a02ab20ce0a8132a0e5312b53a5f23cef5000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+        "v": "0x0",
+        "r": "0xd768f4d808fc1cb0eedca99363b78d9fa42555b4f26cbf5fa48ba8af96bff159",
+        "s": "0x7f4cd55d6d06422ce14f58e72b0f366b479f606d129e4fc959a5eb348c93e888",
+        "type": "0x2",
+        "accessList": [],
+        "maxPriorityFeePerGas": "0x55ae82600",
+        "maxFeePerGas": "0x174876e800",
+        "chainId": "0x1"
+    }"#;
+
+    let tx: Transaction = serde_json::from_str(txs).unwrap();
+    debug!(
+        tx = ?Transaction::decode(&Rlp::new(&tx.rlp())).unwrap(),
+        "decoded payload"
+    );
+    let mock_guard =
+        mount_broadcast_tx_sync_mock_payload_integrity(&test_composer.sequencer, tx.clone()).await;
+
+    test_composer.rollup_nodes["test1"].push_tx(tx).unwrap();
+    tokio::time::timeout(
+        Duration::from_millis(100),
+        mock_guard.wait_until_satisfied(),
+    )
+    .await
+    .expect("mocked sequencer should have received a broadcast message from composer");
+}
+
 /// Deserizalizes the bytes contained in a `tx_sync::Request` to a signed sequencer transaction and
 /// verifies that the contained sequence action is in the given `expected_chain_ids` and
 /// `expected_nonces`.
@@ -215,10 +264,48 @@ async fn mount_broadcast_tx_sync_invalid_nonce_mock(
         .await
 }
 
-fn chain_id_nonce_from_request(request: &Request) -> (Vec<u8>, u32) {
+/// Deserizalizes the bytes contained in a `tx_sync::Request` to a signed sequencer transaction and
+/// verifies that it contains a sequence action with `expected_payload` as its contents.
+async fn mount_broadcast_tx_sync_mock_payload_integrity(
+    server: &MockServer,
+    expected_payload: Transaction,
+) -> MockGuard {
+    let matcher = move |request: &Request| {
+        let sequencer_tx = signed_tx_from_request(request);
+        let sequence_action = sequencer_tx
+            .actions()
+            .get(0)
+            .unwrap()
+            .as_sequence()
+            .unwrap();
+        let payload_rlp = Rlp::new(&sequence_action.data);
+        let payload = Transaction::decode(&payload_rlp).unwrap();
+
+        debug!(?payload, ?expected_payload, "comparing payloads");
+        expected_payload == payload
+    };
+    let jsonrpc_rsp = response::Wrapper::new_with_id(
+        Id::Num(1),
+        Some(tx_sync::Response {
+            code: 0.into(),
+            data: vec![].into(),
+            log: String::new(),
+            hash: tendermint::Hash::Sha256([0; 32]),
+        }),
+        None,
+    );
+
+    Mock::given(matcher)
+        .respond_with(ResponseTemplate::new(200).set_body_json(&jsonrpc_rsp))
+        .up_to_n_times(1)
+        .expect(1)
+        .mount_as_scoped(server)
+        .await
+}
+
+fn signed_tx_from_request(request: &Request) -> SignedTransaction {
     use proto::{
         generated::sequencer::v1alpha1 as raw,
-        native::sequencer::v1alpha1::SignedTransaction,
         Message as _,
     };
 
@@ -230,6 +317,14 @@ fn chain_id_nonce_from_request(request: &Request) -> (Vec<u8>, u32) {
     let signed_tx = SignedTransaction::try_from_raw(raw_signed_tx)
         .expect("can't convert raw signed tx to checked signed tx");
     debug!(?signed_tx, "sequencer mock received signed transaction");
+
+    signed_tx
+}
+
+fn chain_id_nonce_from_request(request: &Request) -> (Vec<u8>, u32) {
+    let signed_tx = signed_tx_from_request(request);
+
+    // validate that the transaction's first action is a sequence action
     let Some(sent_action) = signed_tx.actions().get(0) else {
         panic!("received transaction contained no actions");
     };
