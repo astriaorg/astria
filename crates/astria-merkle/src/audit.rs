5d9ab9ae1b9a5d0b0693fe7c56d7bd3d16cb5796
Richard Janis Goldschmidt <github@aberrat.io>
2023-11-17 11:55:18 +0100

    feat: add an RFC 6962 compliant Merkle tree with flat memory representation (#554)
    ## Summary
    Implement a RFC 6962 compliant merkle tree with flat memory
    representation, remove ct-merkle.
    
    ## Background
    We want to use a merkle tree that minimizes allocations because
    constructing merkle trees is a hot operation that happens every time new
    blocks are generated or verified. Worse, for blocks containing
    transactions for several distinct rollups a merkle Tree is created per
    rollup. Finally, Astria wants a minimal API that is tailored exactly to
    its needs to make audits as simple as possible.
    
    The implementation of this merkle tree library does not allocate space
    for its leaves but only stores leaf and branch hashes in a flat, one
    dimensional array. Furthermore, it only supports sha256 hashes. For
    background reading refer to the following links:
    
    - https://datatracker.ietf.org/doc/html/rfc6962 (reference merkle tree
    this implementation adheres to)
    - https://www.datprotocol.com/deps/0002-hypercore/
    - https://datatracker.ietf.org/doc/rfc7574
    - https://www.ietf.org/archive/id/draft-ietf-mls-protocol-14.html
    - https://mmapped.blog/posts/22-flat-in-order-trees (this is reference
    this implementation builds on in terms of memory representation)
    
    ## Changes
    - Add  an `astria-merkle` crate implementing a flat merkle tree.
    - Rewrite all services/crates in terms of it.
    - Simplify several utility functions by implementing APIs on the merkle
    tree, avoiding allocations.
    
    ## Testing
    - Ensure the merkle tree passes all tests with known roots defined by
    https://github.com/transparency-dev/merkle
    - Add unit and doc tests for all high level APIs.
    - Ensure that all snapshot tests in astria sequencer still pass without
    changing the expdcted hashes
    
    ## Breaking Changelist
    - Sequencer block data and sequencer/rollup blobs are changed, breaking
    compatibility with prior versions of sequencer, sequencer-relayer,
    conductor.

---
+++ crates/astria-merkle/src/audit.rs
@@ -0,0 +1,429 @@
+//! Proving that a leaf is part of a tree.
+
+use sha2::{
+    Digest as _,
+    Sha256,
+};
+
+/// Builder to construct a complex leaf ad-hoc without needing to allocate it.
+///
+/// See `[Audit::with_leaf_builder]` for how to construct it.
+pub struct LeafBuilder<'a, TLeaf, TRoot> {
+    audit: Option<Audit<'a, TLeaf, TRoot>>,
+    hasher: Option<Sha256>,
+}
+
+impl<'a, TLeaf, TRoot> LeafBuilder<'a, TLeaf, TRoot> {
+    /// Finish constructing a leaf.
+    ///
+    /// Returns the internal [`Audit`] with its `TLeaf` typestate set.
+    ///
+    /// # Panics
+    /// This method must only be called once. Calling it again will result
+    /// in a panic.
+    pub fn finish_leaf(&mut self) -> Audit<'a, WithLeafHash, TRoot> {
+        let Audit {
+            proof,
+            root,
+            ..
+        } = self
+            .audit
+            .take()
+            .expect("LeafBuilder::finish_leaf must not be used twice");
+        let leaf_hash = self
+            .hasher
+            .take()
+            .expect("LeafBuilder::finish_leaf must not be used twice")
+            .finalize()
+            .into();
+        Audit {
+            leaf_hash: WithLeafHash {
+                leaf_hash,
+            },
+            proof,
+            root,
+        }
+    }
+
+    /// Write `bytes` into the leaf builder.
+    ///
+    /// # Panics
+    /// This method must not be used after [`LeafBuilder::finish_leaf`] has been called
+    /// and will panic otherwise.
+    pub fn write(&mut self, bytes: &[u8]) -> &mut Self {
+        self.hasher
+            .as_mut()
+            .expect("audit leaf builder must no be used after the leaf is finished")
+            .update(bytes);
+        self
+    }
+}
+
+/// The default type-state for the `Audit` APIs leaf hash source.
+pub struct NoLeafHash;
+
+/// The default type-state for the `Audit` APIs root hash source.
+pub struct NoRoot;
+
+/// The type-state for the `Audit` API after seting a leaf hash.
+pub struct WithLeafHash {
+    leaf_hash: [u8; 32],
+}
+
+/// The type-state for the `Audit` API after setting a root hash.
+pub struct WithRoot {
+    root: [u8; 32],
+}
+
+/// The low level API to perform an audit on a leaf given a proof.
+///
+/// This type follows the type-state builder pattern.
+pub struct Audit<'a, TLeaf = NoLeafHash, TRoot = NoRoot> {
+    leaf_hash: TLeaf,
+    proof: &'a Proof,
+    root: TRoot,
+}
+
+impl<'a> Audit<'a> {
+    /// Construct a new `Audit`.
+    ///
+    /// See [`Proof::audit`] for how to use this.
+    fn new(proof: &'a Proof) -> Self {
+        Self {
+            leaf_hash: NoLeafHash,
+            proof,
+            root: NoRoot,
+        }
+    }
+}
+
+impl<'a, TLeaf, TRoot> Audit<'a, TLeaf, TRoot> {
+    /// Construct an ad-hoc leaf using the [`LeafBuilder`] API.
+    pub fn with_leaf_builder(self) -> LeafBuilder<'a, TLeaf, TRoot> {
+        let hasher = crate::init_leaf_hasher();
+        LeafBuilder {
+            audit: Some(self),
+            hasher: Some(hasher),
+        }
+    }
+
+    /// Audit `leaf` by hashing it.
+    ///
+    /// Returns a new `Audit` with its `TLeaf` type-state to [`WithLeafHash`].
+    pub fn with_leaf(self, leaf: &[u8]) -> Audit<'a, WithLeafHash, TRoot> {
+        self.with_leaf_hash(crate::hash_leaf(leaf))
+    }
+
+    /// Audit `leaf` by directly using the provided `leaf_hash`.
+    ///
+    /// Returns a new `Audit` with its `TLeaf` type-state to [`WithLeafHash`].
+    pub fn with_leaf_hash(self, leaf_hash: [u8; 32]) -> Audit<'a, WithLeafHash, TRoot> {
+        let Self {
+            proof,
+            root,
+            ..
+        } = self;
+        let leaf_hash = WithLeafHash {
+            leaf_hash,
+        };
+        Audit {
+            leaf_hash,
+            proof,
+            root,
+        }
+    }
+
+    /// Perform an audit against the provided `root` hash.
+    ///
+    /// Returns a new `Audit` with its `TRoot` type-state to [`WithRoot`].
+    pub fn with_root(self, root: [u8; 32]) -> Audit<'a, TLeaf, WithRoot> {
+        let Self {
+            proof,
+            leaf_hash,
+            ..
+        } = self;
+        Audit {
+            leaf_hash,
+            proof,
+            root: WithRoot {
+                root,
+            },
+        }
+    }
+}
+
+impl<'a, TRoot> Audit<'a, WithLeafHash, TRoot> {
+    /// Reconstruct the root hash using the leaf hash stored in the [`WithLeafHash`] state.
+    ///
+    /// # Examples
+    /// ```
+    /// let mut tree = astria_merkle::Tree::from_leaves(&[&[1][..], &[2, 2], &[3, 3]]);
+    /// tree.build_leaf().write(&[4, 2]).write(b"answer");
+    /// let root = tree.root();
+    /// let proof = tree.construct_proof(3).expect("leaf 4 is inside the tree");
+    /// let reconstructed_root = proof
+    ///     .audit()
+    ///     .with_leaf_builder()
+    ///     .write(&[4, 2])
+    ///     .write(b"answer")
+    ///     .finish_leaf()
+    ///     .reconstruct_root();
+    /// assert_eq!(root, reconstructed_root);
+    /// ```
+    pub fn reconstruct_root(&self) -> [u8; 32] {
+        let Self {
+            leaf_hash: WithLeafHash {
+                leaf_hash,
+            },
+            proof,
+            ..
+        } = self;
+        proof.reconstruct_root_with_leaf_hash(*leaf_hash)
+    }
+}
+
+impl<'a> Audit<'a, WithLeafHash, WithRoot> {
+    /// Check if the leaf is included in the tree using the internal proof.
+    ///
+    /// This method reconstructs a Merkle tree root starting from the
+    /// leaf hash stored in the [`WithLeafHash`] and audit's internal proof
+    /// and returns if it matches the root hash stored in the `[WithRoot]`
+    /// state.
+    ///
+    /// This method is useful if you need to construct a more complex leaf
+    /// and want to avoid allocating a buffer for it. Prefer `[Proof::is_leaf_in_tree]`
+    /// if the leaf is not complex or already allocated.
+    ///
+    /// # Examples
+    /// ```
+    /// let mut tree = astria_merkle::Tree::from_leaves(&[&[1][..], &[2, 2], &[3, 3]]);
+    /// tree.build_leaf().write(&[4, 2]).write(b"answer");
+    /// let root = tree.root();
+    /// let proof = tree.construct_proof(3).expect("leaf 4 is inside the tree");
+    /// assert!(
+    ///     proof
+    ///         .audit()
+    ///         .with_root(root)
+    ///         .with_leaf_builder()
+    ///         .write(&[4, 2])
+    ///         .write(b"answer")
+    ///         .finish_leaf()
+    ///         .perform()
+    /// );
+    /// ```
+    #[must_use = "verify the audit result"]
+    pub fn perform(&self) -> bool {
+        let Self {
+            leaf_hash: WithLeafHash {
+                leaf_hash,
+            },
+            proof,
+            root: WithRoot {
+                root,
+            },
+        } = self;
+        *root == proof.reconstruct_root_with_leaf_hash(*leaf_hash)
+    }
+}
+
+/// The proof that a node is included in a Merkle tree.
+///
+/// The proof is the concatenation of all sibling hashes required to reconstruct
+/// the Merkle tree from a leaf. This is also called the audit path.
+#[derive(Clone, Debug, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Proof {
+    pub(super) audit_path: Vec<u8>,
+    pub(super) leaf_index: usize,
+    pub(super) tree_size: usize,
+}
+
+impl Proof {
+    /// Returns the audit path of the proof.
+    ///
+    /// This is the concatenation of all hashes to reconstruct merkle tree root
+    /// from the node under proof.
+    #[must_use]
+    #[inline]
+    pub fn audit_path(&self) -> &[u8] {
+        &self.audit_path
+    }
+
+    /// Returns the leaf index
+    #[must_use]
+    #[inline]
+    pub fn leaf_index(&self) -> usize {
+        self.leaf_index
+    }
+
+    /// Returns if the proof is empty.
+    ///
+    /// This can happen if the proof was constructed for a tree containing only
+    /// one leaf.
+    #[must_use]
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.audit_path.is_empty()
+    }
+
+    /// Returns the number of segments in the proof.
+    #[must_use]
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.audit_path.len() / 32
+    }
+
+    /// Returns the size of the tree this proof was derived from.
+    #[must_use]
+    #[inline]
+    pub fn tree_size(&self) -> usize {
+        self.tree_size
+    }
+
+    /// Starts an audit using the [`Audit`] API.
+    ///
+    /// This method is the entry point to the [`Audit`] API to either
+    /// reconstruct a merkle tree root, or to test if a leaf can be verified
+    /// to be part of a tree using the given proof.
+    ///
+    /// It is particularly useful for verifying that a more complex leaf without
+    /// needing to allocate a buffer for it.
+    ///
+    /// Use [`Proof::is_leaf_in_tree`] if the leaf is not complex or already allocated.
+    ///
+    /// # Examples
+    /// ```
+    /// # use astria_merkle::Tree;
+    /// let mut tree = Tree::from_leaves(&[&[1; 32][..], &[4, 4, 4], b"helloworld"]);
+    /// tree.build_leaf()
+    ///     .write(&[42; 1])
+    ///     .write(&[1, 1])
+    ///     .write(&vec![42; 3])
+    ///     .write(b"42");
+    /// let root = tree.root();
+    /// let proof = tree.construct_proof(3).expect("leaf 4 is in the tree");
+    /// assert!(
+    ///     proof
+    ///         .audit()
+    ///         .with_root(root)
+    ///         .with_leaf_builder()
+    ///         .write(&[42; 1])
+    ///         .write(&[1, 1])
+    ///         .write(&vec![42; 3])
+    ///         .write(b"42")
+    ///         .finish_leaf()
+    ///         .perform()
+    /// );
+    /// ```
+    #[must_use = "an audit must be performed to be useful"]
+    pub fn audit(&self) -> Audit {
+        Audit::new(self)
+    }
+
+    /// Walks the audit path to reconstruct the root hash starting from a node hash.
+    ///
+    /// Use this method if the starting point is the hash of a node (like the leaf hash)
+    /// which does not need to be hashed prior to combining with its sibling.
+    ///
+    /// # Examples
+    /// ```
+    /// # use astria_merkle::Tree;
+    /// let mut tree = Tree::new();
+    /// tree.push(&[1; 32]);
+    /// tree.push(&[2; 32]);
+    /// tree.push(&[3; 32]);
+    /// tree.push(&[4; 32]);
+    /// tree.push(&[5; 32]);
+    /// tree.push(&[6; 32]);
+    /// tree.push(&[7; 32]);
+    /// tree.push(&[8; 32]);
+    /// let root = tree.root();
+    /// let proof = tree
+    ///     .construct_proof(6)
+    ///     .expect("leaf 7 must be inside the tree");
+    /// let leaf_hash = tree.leaf(6).expect("leaf 7 must be inside the tree");
+    /// let reconstructed_root = proof.reconstruct_root_with_leaf_hash(leaf_hash);
+    /// assert_eq!(root, reconstructed_root);
+    /// ```
+    #[must_use]
+    pub fn reconstruct_root_with_leaf_hash(&self, leaf_hash: [u8; 32]) -> [u8; 32] {
+        let Self {
+            audit_path,
+            leaf_index,
+            tree_size,
+        } = self;
+        let mut i = crate::leaf_index_to_tree_index(*leaf_index);
+        let mut acc = leaf_hash;
+        for sibling in audit_path.chunks(32) {
+            let parent = crate::complete_parent(i, *tree_size);
+            if parent > i {
+                acc = crate::combine(&acc, sibling);
+            } else {
+                acc = crate::combine(sibling, &acc);
+            }
+            i = parent;
+        }
+        acc
+    }
+
+    /// Walks the audit path to reconstruct the root hash starting from a node.
+    ///
+    /// Use this method if the starting point is a leaf, which will
+    /// be hashed prior to combining with its sibling.
+    ///
+    /// Use the [`Audit::reconstruct_root`] via [`Proof::audit`] if the leaf
+    /// is more complex and you want to avoid allocating a buffer for it.
+    ///
+    /// # Examples
+    /// ```
+    /// let mut tree = astria_merkle::Tree::new();
+    /// tree.push(&[1; 32]);
+    /// tree.push(&[2; 32]);
+    /// tree.push(&[3; 32]);
+    /// tree.push(&[4; 32]);
+    /// tree.push(&[5; 32]);
+    /// tree.push(&[6; 32]);
+    /// tree.push(&[7; 32]);
+    /// tree.push(&[8; 32]);
+    /// let root = tree.root();
+    /// let proof = tree
+    ///     .construct_proof(6)
+    ///     .expect("leaf 7 must be inside the tree");
+    /// let reconstructed_root = proof.reconstruct_root_with_leaf(&[7_u8; 32]);
+    /// assert_eq!(root, reconstructed_root);
+    /// ```
+    #[must_use]
+    pub fn reconstruct_root_with_leaf(&self, leaf: &[u8]) -> [u8; 32] {
+        self.audit().with_leaf(leaf).reconstruct_root()
+    }
+
+    /// Returns if `leaf` is part of the merkle tree identified by its `root_hash`.
+    ///
+    /// This is a utility function that walks the audit path in `Proof` starting
+    /// from `leaf` and compares with `root_hash`.
+    ///
+    /// Use the [`Audit`] API via [`Proof::audit`] if `leaf` is more complex
+    /// and you want to avoid allocating a buffer for it.
+    ///
+    /// # Examples
+    /// ```
+    /// let mut tree = astria_merkle::Tree::new();
+    /// tree.push(&[1; 32]);
+    /// tree.push(&[2; 32]);
+    /// tree.push(&[3; 32]);
+    /// tree.push(&[4; 32]);
+    /// tree.push(&[5; 32]);
+    /// tree.push(&[6; 32]);
+    /// tree.push(&[7; 32]);
+    /// tree.push(&[8; 32]);
+    /// let root = tree.root();
+    /// let proof = tree
+    ///     .construct_proof(6)
+    ///     .expect("leaf 7 must be inside the tree");
+    /// assert!(proof.verify(&[7; 32], root));
+    /// ```
+    #[must_use]
+    pub fn verify(&self, leaf: &[u8], root_hash: [u8; 32]) -> bool {
+        self.audit().with_leaf(leaf).with_root(root_hash).perform()
+    }
+}
