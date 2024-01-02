e5cda5b6a954e17ccd652f357e93a743d83d580b
Richard Janis Goldschmidt <github@aberrat.io>
2024-01-02 17:17:16 +0100

    clippy

--- crates/astria-telemetry/src/display.rs
+++ crates/astria-telemetry/src/display.rs
@@ -8,16 +8,16 @@
 /// let signature = vec![1u8, 2, 3, 4, 5, 6, 7, 8];
 /// tracing::info!(signature = %display::hex(&signature), "received signature");
 /// ```
-pub fn hex<T: AsRef<[u8]>>(bytes: &T) -> DisplayHex<'_> {
-    DisplayHex(bytes.as_ref())
+pub fn hex<T: AsRef<[u8]>>(bytes: &T) -> Hex<'_> {
+    Hex(bytes.as_ref())
 }
 
 /// A newtype wrapper of a byte slice that implements [`std::fmt::Display`].
 ///
 /// To be used in tracing contexts. See the [`self::hex`] utility.
-pub struct DisplayHex<'a>(pub &'a [u8]);
+pub struct Hex<'a>(pub &'a [u8]);
 
-impl<'a> std::fmt::Display for DisplayHex<'a> {
+impl<'a> std::fmt::Display for Hex<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for byte in self.0 {
             f.write_fmt(format_args!("{byte:02x}"))?;
