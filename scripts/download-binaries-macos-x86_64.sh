7f2f27cd3bb5f6eb8218ae11e02b445648df76d2
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-02-19 16:51:05 -0500

    big env file, all binary download

--- scripts/download-binaries-macos-x86_64.sh
+++ scripts/download-binaries-macos-x86_64.sh
@@ -3,10 +3,10 @@
 # donwload binaries for macos arm64
 
 package_urls=(
-    "https://github.com/cometbft/cometbft/releases/download/v0.37.4/cometbft_0.37.4_darwin_arm64.tar.gz"
-    "https://github.com/astriaorg/astria/releases/download/sequencer-v0.9.0/astria-sequencer-aarch64-apple-darwin.tar.gz"
-    "https://github.com/astriaorg/astria/releases/download/composer-v0.4.0/astria-composer-aarch64-apple-darwin.tar.gz"
-    "https://github.com/astriaorg/astria/releases/download/conductor-v0.12.0/astria-conductor-aarch64-apple-darwin.tar.gz"
+    "https://github.com/cometbft/cometbft/releases/download/v0.37.4/cometbft_0.37.4_darwin_amd64.tar.gz"
+    "https://github.com/astriaorg/astria/releases/download/sequencer-v0.9.0/astria-sequencer-x86_64-apple-darwin.tar.gz"
+    "https://github.com/astriaorg/astria/releases/download/composer-v0.4.0/astria-composer-x86_64-apple-darwin.tar.gz"
+    "https://github.com/astriaorg/astria/releases/download/conductor-v0.12.0/astria-conductor-x86_64-apple-darwin.tar.gz"
 )
 package_names=(
     "cometbft"
