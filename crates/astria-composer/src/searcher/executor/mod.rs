9979c7259788c52e6022480fe914dc7da7e0010f
Jordan Oroshiba <jordan@astria.org>
2024-03-08 13:59:46 -0800

    format

--- crates/astria-composer/src/searcher/executor/mod.rs
+++ crates/astria-composer/src/searcher/executor/mod.rs
@@ -194,7 +194,7 @@ impl Executor {
         let block_timer = time::sleep(self.block_time);
         tokio::pin!(block_timer);
         let mut bundle_factory = BundleFactory::new(self.max_bytes_per_bundle);
-        let reset_time = || { Instant::now() + self.block_time };
+        let reset_time = || Instant::now() + self.block_time;
 
         loop {
             select! {
