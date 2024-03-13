9bcc0c7844c824041bceae833130262f8f135a71
Richard Janis Goldschmidt <github@aberrat.io>
2024-03-13 16:57:45 +0100

    add retry to sequencer block fetch

--- crates/astria-conductor/src/sequencer/client.rs
+++ crates/astria-conductor/src/sequencer/client.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use astria_core::{
     generated::sequencer::v1alpha1::{
         sequencer_service_client::SequencerServiceClient,
@@ -17,7 +19,11 @@ use tonic::transport::{
     Endpoint,
     Uri,
 };
-use tracing::instrument;
+use tracing::{
+    instrument,
+    warn,
+    Instrument,
+};
 
 #[derive(Clone)]
 pub(crate) struct SequencerGrpcClient {
@@ -49,16 +55,40 @@ impl SequencerGrpcClient {
         height: u64,
         rollup_id: RollupId,
     ) -> eyre::Result<FilteredSequencerBlock> {
-        let req = GetFilteredSequencerBlockRequest {
-            height,
-            rollup_ids: vec![rollup_id.to_vec()],
-        };
-        let raw_block = self
-            .inner
-            .get_filtered_sequencer_block(req)
-            .await
-            .wrap_err("failed fetching sequencer block information for rollup")?
-            .into_inner();
+        let span = tracing::Span::current();
+        let retry_cfg = tryhard::RetryFutureConfig::new(u32::MAX)
+            .exponential_backoff(Duration::from_millis(100))
+            .max_delay(Duration::from_secs(5))
+            .on_retry(
+                |attempt: u32, next_delay: Option<Duration>, error: &tonic::Status| {
+                    let wait_duration = next_delay
+                        .map(humantime::format_duration)
+                        .map(tracing::field::display);
+                    warn!(
+                        parent: &span,
+                        attempt,
+                        wait_duration,
+                        error = error as &dyn std::error::Error,
+                        "attempt to grab sequencer block failed; retrying after backoff",
+                    );
+                    futures::future::ready(())
+                },
+            );
+
+        let client = self.inner.clone();
+        let raw_block = tryhard::retry_fn(|| {
+            let mut client = client.clone();
+            let req = GetFilteredSequencerBlockRequest {
+                height,
+                rollup_ids: vec![rollup_id.to_vec()],
+            };
+            async move { client.get_filtered_sequencer_block(req).await }
+        })
+        .with_config(retry_cfg)
+        .in_current_span()
+        .await
+        .wrap_err("failed fetching filtered block after a lot of retries, bailing")?
+        .into_inner();
         FilteredSequencerBlock::try_from_raw(raw_block)
             .wrap_err("failed validating filtered block response")
     }
