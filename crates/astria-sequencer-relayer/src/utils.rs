da01133fe3ed62e082f9f626d5d6f4f4969172a4
Richard Janis Goldschmidt <github@aberrat.io>
2024-03-20 23:02:29 +0100

    fix(conductor): don't panic while shutting down (#846)
    ## Summary
    Fixes conductor panicking during shutdown.
    
    ## Background
    Conductor was using a tokio `LocalSet` together with an `Rc` to run its
    shutdown logic. This was entirely unnecessary and panicky (since the
    `Rc` was cloned into multiple tasks, which was obviously wrong).
    
    Note that this PR does not address conductor ignoring `SIGTERM` during
    initialization.
    
    ## Changes
    - Simplify the awkward `LocalSet` + `Rc` to shutdown conductor tasks
    - Simplify reporting task failure and exist
    
    ## Testing
    This needs to be tested end to end (since conductor receives an external
    signal).
    
    ## Related Issues
    Closes #828

--- crates/astria-sequencer-relayer/src/utils.rs
+++ crates/astria-sequencer-relayer/src/utils.rs
@@ -7,7 +7,7 @@ use tokio::task::JoinError;
 pub(crate) fn flatten<T>(res: Result<eyre::Result<T>, JoinError>) -> eyre::Result<T> {
     match res {
         Ok(Ok(val)) => Ok(val),
-        Ok(Err(err)) => Err(err).wrap_err("fask returned with error"),
+        Ok(Err(err)) => Err(err).wrap_err("task returned with error"),
         Err(err) => Err(err).wrap_err("task panicked"),
     }
 }
