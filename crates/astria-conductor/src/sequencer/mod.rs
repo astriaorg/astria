75a7548912e07c1a1def7f4f4c1180df922545d3
elizabeth <elizabethjbinks@gmail.com>
2024-03-26 13:50:27 -0400

    remove conductor changes

--- crates/astria-conductor/src/sequencer/mod.rs
+++ crates/astria-conductor/src/sequencer/mod.rs
@@ -25,6 +25,7 @@ use tracing::{
     error,
     info,
     instrument,
+    trace,
     warn,
 };
 
@@ -128,7 +129,7 @@ impl Reader {
                             }
 
                             executor::channel::TrySendError::NoPermits(block) => {
-                                debug!("executor channel is full; scheduling block and stopping block fetch until a slot opens up");
+                                trace!("executor channel is full; scheduling block and stopping block fetch until a slot opens up");
                                 enqueued_block = executor.clone().send_soft_block_owned(block).boxed().fuse();
                             }
                         }
