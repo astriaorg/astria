992fc1c169e839db449813a8843f25dfb4bc85a4
lilyjjo <lillianjanjohnson@gmail.com>
2024-04-09 11:17:42 -0400

    fix(sequecer): is_proposer check now considers address

--- crates/astria-sequencer/src/app.rs
+++ crates/astria-sequencer/src/app.rs
@@ -102,14 +102,14 @@ const MAX_SEQUENCE_DATA_BYTES_PER_BLOCK: usize = 256_000;
 pub(crate) struct App {
     state: InterBlockState,
 
-    // set to true when `prepare_proposal` is called, indicating we are the proposer for this
-    // block. set to false when `process_proposal` is called, as it's called during the prevote
-    // phase for that block.
+    // The validator address in cometbft being used to sign votes.
     //
-    // if true, `process_proposal` is not executed, as this means we are the proposer of that
-    // block, and we have already executed the transactions for the block during
-    // `prepare_proposal`, and re-executing them would cause failure.
-    is_proposer: bool,
+    // Used to avoid executing a block in both `prepare_proposal` and `process_proposal`. It
+    // is set in `prepare_proposal` from information sent in from cometbft and can change.
+    // In `process_proposal` we check if we prepared the proposal, and if so, we clear the
+    // value and we skip re-execution of the block's transactions to avoid failures caused by
+    // re-execution.
+    signing_address: Option<account::Id>,
 
     // This is set to the executed hash of the proposal during `process_proposal`
     //
@@ -156,7 +156,7 @@ impl App {
 
         Self {
             state,
-            is_proposer: false,
+            signing_address: None,
             executed_proposal_hash: Hash::default(),
             execution_result: HashMap::new(),
             processed_txs: 0,
@@ -234,7 +234,7 @@ impl App {
         prepare_proposal: abci::request::PrepareProposal,
         storage: Storage,
     ) -> anyhow::Result<abci::response::PrepareProposal> {
-        self.is_proposer = true;
+        self.signing_address = Some(prepare_proposal.proposer_address);
         self.update_state_for_new_round(&storage);
 
         let (signed_txs, txs_to_include) = self.execute_block_data(prepare_proposal.txs).await;
@@ -266,16 +266,19 @@ impl App {
         // if we proposed this block (ie. prepare_proposal was called directly before this), then
         // we skip execution for this `process_proposal` call.
         //
-        // if we didn't propose this block, `self.is_proposer` will be `false`, so
-        // we will execute the block as normal.
-        if self.is_proposer {
-            debug!("skipping process_proposal as we are the proposer for this block");
-            self.is_proposer = false;
-            self.executed_proposal_hash = process_proposal.hash;
-            return Ok(());
+        // if we didn't propose this block, `self.signing_address` will be None or a differnet
+        // value, so we will execute the block as normal.
+        if let Some(id) = self.signing_address {
+            if id == process_proposal.proposer_address {
+                debug!("skipping process_proposal as we are the proposer for this block");
+                self.signing_address = None;
+                self.executed_proposal_hash = process_proposal.hash;
+                return Ok(());
+            }
+            debug!("signing address was set but we're not the proposer, shouldn't happen");
+            self.signing_address = None;
         }
 
-        self.is_proposer = false;
         self.update_state_for_new_round(&storage);
 
         let mut txs = VecDeque::from(process_proposal.txs);
