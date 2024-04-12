46f4ab1a761eae32e6dcf75f5801f90653a33ac3
Jordan Oroshiba <jordan@astria.org>
2024-04-12 13:18:40 -0700

    feat(chart): create hermes chart

--- charts/celestia-local/files/scripts/init-celestia-appd.sh
+++ charts/celestia-local/files/scripts/init-celestia-appd.sh
@@ -2,19 +2,24 @@
 
 set -o errexit -o nounset
 
-# if app config already exists then we have already ran initialization.
-if [ -f "$home_dir/config/config.toml" ]; then
-  exit 0
-fi
+rm -rf $home_dir/*
 
 celestia-appd init "$chainid" \
   --chain-id "$chainid" \
   --home "$home_dir"
 
-celestia-appd keys add \
-  "$validator_key_name" \
-  --keyring-backend="$keyring_backend" \
-  --home "$home_dir"
+if [ -n $validator_mnemonic ]; then
+  echo $validator_mnemonic | celestia-appd keys add \
+    "$validator_key_name" \
+    --home "$home_dir" \
+    --keyring-backend="$keyring_backend" \
+    --recover
+else
+  celestia-appd keys add \
+    "$validator_key_name" \
+    --keyring-backend="$keyring_backend" \
+    --home "$home_dir"
+fi
 
 validator_key=$(celestia-appd keys show "$validator_key_name" -a --keyring-backend="$keyring_backend" --home "$home_dir")
 celestia-appd add-genesis-account \
@@ -27,6 +32,6 @@ celestia-appd gentx \
   "$validator_stake" \
   --keyring-backend="$keyring_backend" \
   --chain-id "$chainid" \
-  --home "$home_dir" \
+  --home "$home_dir"
 
 celestia-appd collect-gentxs --home "$home_dir"
