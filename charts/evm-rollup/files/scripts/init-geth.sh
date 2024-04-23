57cbcffed6263d5a895b9887c5f34166056865c4
Jordan Oroshiba <jordan@astria.org>
2024-04-22 22:15:49 -0700

    fix geth init script

--- charts/evm-rollup/files/scripts/init-geth.sh
+++ charts/evm-rollup/files/scripts/init-geth.sh
@@ -2,14 +2,18 @@
 
 set -o errexit -o nounset
 
-if [ -d "$($data_dir/)" ]; then
+if [ ! -d "$data_dir/" ]; then
   echo "Initializing geth db..."
+
+  cp /scripts/geth-genesis.json $home_dir/genesis.json
 
   exec geth --datadir "$data_dir/" --db.engine {{ .Values.config.rollup.dbEngine }} \
     {{ if not .Values.config.rollup.archiveNode }}--state.scheme=path {{- end }} \
-    init /scripts/geth-genesis.json
-elif [! $(cmp -s "/scripts/geth-genesis.json" "/genesis.json")]; then
+    init $home_dir/genesis.json
+elif ! cmp -s "/scripts/geth-genesis.json" "$home_dir/genesis.json"; then
   echo "Geth DB already initialized, but genesis file upgraded..."
 
-  exec geth --datadir "$data_dir/" init /scripts/geth-genesis.json
+  cp /scripts/geth-genesis.json $home_dir/genesis.json
+
+  exec geth --datadir "$data_dir/" init $home_dir/genesis.json
 fi
