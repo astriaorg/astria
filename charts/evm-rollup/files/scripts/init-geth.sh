55f36418ae91f98b96151b2961e074353e46217f
Jordan Oroshiba <jordan@astria.org>
2024-04-22 17:13:00 -0700

    some evm chart updates

--- charts/evm-rollup/files/scripts/init-geth.sh
+++ charts/evm-rollup/files/scripts/init-geth.sh
@@ -2,10 +2,14 @@
 
 set -o errexit -o nounset
 
-if [ -z "$(ls -A $data_dir/)" ]; then
+if [ -d "$($data_dir/)" ]; then
   echo "Initializing geth db..."
 
   exec geth --datadir "$data_dir/" --db.engine {{ .Values.config.rollup.dbEngine }} \
     {{ if not .Values.config.rollup.archiveNode }}--state.scheme=path {{- end }} \
     init /scripts/geth-genesis.json
+elif [! $(cmp -s "/scripts/geth-genesis.json" "/genesis.json")]; then
+  echo "Geth DB already initialized, but genesis file upgraded..."
+
+  exec geth --datadir "$data_dir/" init /scripts/geth-genesis.json
 fi
