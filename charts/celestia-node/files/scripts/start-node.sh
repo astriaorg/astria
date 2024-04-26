2ca1cf4226ffc8667a369fe4af3b70596be9f099
Jordan Oroshiba <jordan@astria.org>
2024-04-26 15:16:46 -0700

    default to secured for node but allow disable

--- charts/celestia-node/files/scripts/start-node.sh
+++ charts/celestia-node/files/scripts/start-node.sh
@@ -3,13 +3,29 @@
 set -o errexit -o nounset -o pipefail
 {{- $isCustomNetwork := eq .Values.config.network "custom" }}
 
+{{- if .Values.config.tokenAuthLevel }}
+function set_token() {
+  # NOTE - this is a hack to give access to a token generated on startup to people with ssh access
+  TOKEN=$(/bin/celestia {{ .Values.config.type }} auth {{ .Values.config.tokenAuthLevel }} --node.store "/celestia")
+
+  mkdir -p /celestia/token
+  echo "$TOKEN" > /celestia/token/token.key
+}
+
+if [ ! -f /celestia/token/token.key ]; then
+  set_token
+fi
+{{- end }}
+
 {{- if $isCustomNetwork }}
 export CELESTIA_CUSTOM=$CELESTIA_CUSTOM_TO_BE
 {{- end }}
 
 exec /bin/celestia {{ .Values.config.type }} start \
   --node.store /celestia \
-  --rpc.skip-auth \
+  {{- if not .Values.config.tokenAuthLevel }}
+  --rpc.skipAuth \
+  {{- end }}
   {{- if not $isCustomNetwork }}
   --core.ip {{ .Values.config.coreIp }} \
   --core.grpc.port "{{ .Values.config.coreGrpcPort }}" \
