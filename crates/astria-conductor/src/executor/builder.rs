1fa5260c7985ab98c4f2b10568c0d2375c50657d
Richard Janis Goldschmidt <github@aberrat.io>
2024-04-28 14:50:51 +0200

    fix(conductor): only execute firm blocks if at soft head

--- crates/astria-conductor/src/executor/builder.rs
+++ crates/astria-conductor/src/executor/builder.rs
@@ -13,10 +13,10 @@ use super::{
     Handle,
     StateNotInit,
 };
+use crate::config::CommitLevel;
 
 pub(crate) struct Builder {
-    pub(crate) with_firm: bool,
-    pub(crate) with_soft: bool,
+    pub(crate) mode: CommitLevel,
     pub(crate) rollup_address: String,
     pub(crate) shutdown: CancellationToken,
 }
@@ -24,10 +24,9 @@ pub(crate) struct Builder {
 impl Builder {
     pub(crate) fn build(self) -> eyre::Result<(Executor, Handle)> {
         let Self {
+            mode,
             rollup_address,
             shutdown,
-            with_firm,
-            with_soft,
         } = self;
 
         let rollup_address = rollup_address
@@ -36,7 +35,7 @@ impl Builder {
 
         let mut firm_block_tx = None;
         let mut firm_block_rx = None;
-        if with_firm {
+        if mode.is_with_firm() {
             let (tx, rx) = mpsc::channel(16);
             firm_block_tx = Some(tx);
             firm_block_rx = Some(rx);
@@ -44,7 +43,7 @@ impl Builder {
 
         let mut soft_block_tx = None;
         let mut soft_block_rx = None;
-        if with_soft {
+        if mode.is_with_soft() {
             let (tx, rx) = super::soft_block_channel();
             soft_block_tx = Some(tx);
             soft_block_rx = Some(rx);
@@ -53,6 +52,8 @@ impl Builder {
         let (state_tx, state_rx) = state::channel();
 
         let executor = Executor {
+            mode,
+
             firm_blocks: firm_block_rx,
             soft_blocks: soft_block_rx,
 
--- crates/astria-conductor/src/executor/tests.rs
+++ crates/astria-conductor/src/executor/tests.rs
@@ -11,12 +11,14 @@ use astria_core::{
 use bytes::Bytes;
 
 use super::{
+    should_execute_firm_block,
     state::{
         StateReceiver,
         StateSender,
     },
     RollupId,
 };
+use crate::config::CommitLevel;
 
 const ROLLUP_ID: RollupId = RollupId::new([42u8; 32]);
 
@@ -79,7 +81,7 @@ state",
 }
 
 #[test]
-fn foo() {
+fn execute_block_contract_violation() {
     use super::ExecutionKind::{
         Firm,
         Soft,
@@ -154,5 +156,53 @@ fn foo() {
             soft: 3,
         },
         5,
+    );
+}
+
+#[test]
+fn should_execute_firm() {
+    use CommitLevel::{
+        FirmOnly,
+        SoftAndFirm,
+        SoftOnly,
+    };
+
+    assert!(
+        should_execute_firm_block(1, 1, FirmOnly),
+        "firm-mode conductors should always execute firm blocks"
+    );
+    assert!(
+        should_execute_firm_block(0, 1, FirmOnly),
+        "firm-mode conductors should always execute firm blocks"
+    );
+    assert!(
+        should_execute_firm_block(1, 0, FirmOnly),
+        "firm-mode conductors should always execute firm blocks"
+    );
+    assert!(
+        !should_execute_firm_block(1, 1, SoftOnly),
+        "soft-mode conductors should never execute firm blocks"
+    );
+    assert!(
+        !should_execute_firm_block(0, 1, SoftOnly),
+        "soft-mode conductors should never execute firm blocks"
+    );
+    assert!(
+        !should_execute_firm_block(1, 0, SoftOnly),
+        "soft-mode conductors should never execute firm blocks"
+    );
+    assert!(
+        should_execute_firm_block(1, 1, SoftAndFirm),
+        "firm-and-soft-mode conductors should execute firm blocks if soft and firm numbers match"
+    );
+    assert!(
+        !should_execute_firm_block(0, 1, SoftAndFirm),
+        "firm-and-soft-mode conductors should not execute firm blocks if soft and firm numbers \
+         don't match"
+    );
+    assert!(
+        !should_execute_firm_block(1, 0, SoftAndFirm),
+        "firm-and-soft-mode conductors should not execute firm blocks if soft and firm numbers \
+         don't match"
     );
 }
