f4b621441b02af815adc487c342ab86027936bda
elizabeth <elizabethjbinks@gmail.com>
2024-05-09 16:42:18 -0400

    add snapshot test with every action, add comments

--- crates/astria-sequencer/src/app/tests_breaking_changes.rs
+++ crates/astria-sequencer/src/app/tests_breaking_changes.rs
@@ -1,3 +1,13 @@
+//! The tests in this file are snapshot tests that are expected to break
+//! if breaking changes are made to the application, in particular,
+//! any changes that can affect the state tree and thus the app hash.
+//!
+//! If these tests break due to snapshot mismatches, you can update the snapshots
+//! with `cargo insta review`, but you MUST mark the respective PR as breaking.
+//!
+//! Note: there are two actions not tested here: `Ics20Withdrawal` and `IbcRelay`.
+//! These are due to the extensive setup needed to test them.
+//! If changes are made to the execution results of these actions, manual testing is required.
 use std::collections::HashMap;
 
 use astria_core::{
@@ -9,6 +19,7 @@ use astria_core::{
     protocol::transaction::v1alpha1::{
         action::{
             BridgeLockAction,
+            IbcRelayerChangeAction,
             SequenceAction,
             TransferAction,
         },
@@ -37,6 +48,7 @@ use crate::{
         initialize_app,
         initialize_app_with_storage,
         BOB_ADDRESS,
+        CAROL_ADDRESS,
     },
     asset::get_native_asset,
     bridge::state_ext::StateWriteExt as _,
@@ -65,6 +77,9 @@ async fn app_finalize_block_snapshot() {
         .put_bridge_account_asset_id(&bridge_address, &asset_id)
         .unwrap();
     app.apply(state_tx);
+
+    // the state changes must be committed, as `finalize_block` will execute the
+    // changes on the latest snapshot, not the app's `StateDelta`.
     app.prepare_commit(storage.clone()).await.unwrap();
     app.commit(storage.clone()).await;
 
@@ -123,37 +138,24 @@ async fn app_finalize_block_snapshot() {
     insta::assert_json_snapshot!(app.app_hash.as_bytes());
 }
 
+// Note: this tests every action except for `Ics20Withdrawal` and `IbcRelay`.
+//
+// If new actions are added to the app, they must be added to this test,
+// and the respective PR must be marked as breaking.
 #[tokio::test]
-async fn app_execute_transaction_transfer_snapshot() {
-    let mut app = initialize_app(None, vec![]).await;
-
-    let (alice_signing_key, _) = get_alice_signing_key_and_address();
-    let bob_address = address_from_hex_string(BOB_ADDRESS);
-    let value = 333_333;
-    let tx = UnsignedTransaction {
-        params: TransactionParams {
-            nonce: 0,
-            chain_id: "test".to_string(),
+async fn app_execute_transaction_with_every_action_snapshot() {
+    use astria_core::{
+        primitive::v1::asset,
+        protocol::transaction::v1alpha1::action::{
+            FeeAssetChangeAction,
+            InitBridgeAccountAction,
+            SudoAddressChangeAction,
         },
-        actions: vec![
-            TransferAction {
-                to: bob_address,
-                amount: value,
-                asset_id: get_native_asset().id(),
-                fee_asset_id: get_native_asset().id(),
-            }
-            .into(),
-        ],
     };
 
-    let signed_tx = tx.into_signed(&alice_signing_key);
-    app.execute_transaction(signed_tx).await.unwrap();
-    insta::assert_json_snapshot!(app.app_hash.as_bytes());
-}
-
-#[tokio::test]
-async fn app_execute_transaction_validator_update_snapshot() {
     let (alice_signing_key, alice_address) = get_alice_signing_key_and_address();
+    let bob_address = address_from_hex_string(BOB_ADDRESS);
+    let carol_address = address_from_hex_string(CAROL_ADDRESS);
 
     let genesis_state = GenesisState {
         accounts: default_genesis_accounts(),
@@ -166,18 +168,70 @@ async fn app_execute_transaction_validator_update_snapshot() {
     };
     let mut app = initialize_app(Some(genesis_state), vec![]).await;
 
+    // setup for ValidatorUpdate action
     let pub_key = tendermint::public_key::PublicKey::from_raw_ed25519(&[1u8; 32]).unwrap();
     let update = tendermint::validator::Update {
         pub_key,
         power: 100u32.into(),
     };
 
+    // setup for BridgeLockAction
+    let bridge_address = Address::from([99; 20]);
+    let rollup_id = RollupId::from_unhashed_bytes(b"testchainid");
+    let asset_id = get_native_asset().id();
+    let mut state_tx = StateDelta::new(app.state.clone());
+    state_tx.put_bridge_account_rollup_id(&bridge_address, &rollup_id);
+    state_tx
+        .put_bridge_account_asset_id(&bridge_address, &asset_id)
+        .unwrap();
+    app.apply(state_tx);
+
     let tx = UnsignedTransaction {
         params: TransactionParams {
             nonce: 0,
             chain_id: "test".to_string(),
         },
-        actions: vec![Action::ValidatorUpdate(update.clone())],
+        actions: vec![
+            TransferAction {
+                to: bob_address,
+                amount: 333_333,
+                asset_id: get_native_asset().id(),
+                fee_asset_id: get_native_asset().id(),
+            }
+            .into(),
+            SequenceAction {
+                rollup_id: RollupId::from_unhashed_bytes(b"testchainid"),
+                data: b"hello world".to_vec(),
+                fee_asset_id: get_native_asset().id(),
+            }
+            .into(),
+            Action::ValidatorUpdate(update.clone()),
+            IbcRelayerChangeAction::Addition(bob_address).into(),
+            IbcRelayerChangeAction::Addition(carol_address).into(),
+            IbcRelayerChangeAction::Removal(bob_address).into(),
+            // TODO: should fee assets be stored in state?
+            FeeAssetChangeAction::Addition(asset::Id::from("test-0".to_string())).into(),
+            FeeAssetChangeAction::Addition(asset::Id::from("test-1".to_string())).into(),
+            FeeAssetChangeAction::Removal(asset::Id::from("test-0".to_string())).into(),
+            InitBridgeAccountAction {
+                rollup_id: RollupId::from_unhashed_bytes(b"testchainid"),
+                asset_id: get_native_asset().id(),
+                fee_asset_id: get_native_asset().id(),
+            }
+            .into(),
+            BridgeLockAction {
+                to: bridge_address,
+                amount: 100,
+                asset_id: get_native_asset().id(),
+                fee_asset_id: get_native_asset().id(),
+                destination_chain_address: "nootwashere".to_string(),
+            }
+            .into(),
+            SudoAddressChangeAction {
+                new_address: bob_address,
+            }
+            .into(),
+        ],
     };
 
     let signed_tx = tx.into_signed(&alice_signing_key);
