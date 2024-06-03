35d69a64467a555b305a2a20bc28e084b2536082
noot <36753753+noot@users.noreply.github.com>
2024-06-03 15:42:35 -0400

    refactor(sequencer): remove mint module (#1134)
    ## Summary
    remove the mint module and associated types.
    
    ## Background
    we never used it, and not something we want in production anyways.
    
    ## Changes
    - remove the mint module and associated types.
    
    ## Testing
    n/a, code was removed
    
    
    ## Breaking changes
    this removes the mint action, which is technically a breaking change,
    however it was gated under a feature flag in sequencer which was never
    enabled on any network afaik. so I don't think this change affects any
    running network.

--- crates/astria-sequencer/src/lib.rs
+++ crates/astria-sequencer/src/lib.rs
@@ -13,8 +13,6 @@ pub(crate) mod grpc;
 pub(crate) mod ibc;
 mod mempool;
 pub mod metrics_init;
-#[cfg(feature = "mint")]
-pub(crate) mod mint;
 pub(crate) mod proposal;
 pub(crate) mod sequence;
 mod sequencer;
