7765ca3c25db09f42652e3a185bebf3247399bf5
elizabeth <elizabethjbinks@gmail.com>
2024-06-05 00:19:05 -0400

    allow clippy::all on generated

--- crates/astria-bridge-withdrawer/src/withdrawer/ethereum/generated/mod.rs
+++ crates/astria-bridge-withdrawer/src/withdrawer/ethereum/generated/mod.rs
@@ -1,9 +1,12 @@
 #![allow(warnings)]
 #![allow(clippy::all)]
 
+#[allow(clippy::all)]
 pub(crate) mod astria_withdrawer_interface;
 
 #[cfg(test)]
+#[allow(clippy::all)]
 pub(crate) mod astria_mintable_erc20;
 #[cfg(test)]
+#[allow(clippy::all)]
 pub(crate) mod astria_withdrawer;
