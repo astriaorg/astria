d64d458daa4084811b39e82ab28b43513627f2cf
noot <36753753+noot@users.noreply.github.com>
2024-06-24 12:18:59 -0400

    feat(sequencer)!: implement `Ics20TransferDepositMemo` format for incoming ics20 transfers to bridge accounts (#1202)
    ## Summary
    implement `Ics20TransferDepositMemo` format and handling for incoming
    ics20 transfers to bridge accounts, which requires the memo be of a
    specific format instead of assuming the entire memo is the destination
    chain address.
    
    ## Background
    the memo being just a destination chain address is not good for UX and
    could lead to error.
    
    ## Changes
    - implement `Ics20TransferDepositMemo` type which contains the
    destination `rollup_address`
    - add stateless check that the `FungibleTokenPacketData` (which is the
    ics20 transfer packet itself) is not empty or too long
    -  parsing and handling of the memo for incoming ics20 transfers
    
    ## Testing
    unit tests
    
    ## Breaking Changelist
    - the validation of the ICS20 memo is different, so this will break
    previously sent ICS20 transfers to bridge accounts.

--- crates/astria-core/src/bridge.rs
+++ crates/astria-core/src/bridge.rs
@@ -1,5 +1,7 @@
 use crate::primitive::v1::Address;
 
+/// Memo format for a ICS20 withdrawal from the rollup which is sent to
+/// an external IBC-enabled chain.
 #[derive(Debug, Clone)]
 #[cfg_attr(
     feature = "serde",
@@ -10,5 +12,26 @@ pub struct Ics20WithdrawalFromRollupMemo {
     pub memo: String,
     pub bridge_address: Address,
     pub block_number: u64,
+    #[cfg_attr(
+        feature = "serde",
+        serde(
+            serialize_with = "crate::serde::base64_serialize",
+            deserialize_with = "crate::serde::base64_deserialize_array"
+        )
+    )]
     pub transaction_hash: [u8; 32],
+}
+
+/// Memo format for a ICS20 transfer to Astria which is sent to a
+/// bridge account, which will then be deposited into the rollup.
+#[derive(Debug, Clone)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize),
+    derive(serde::Deserialize),
+    serde(rename_all = "camelCase", deny_unknown_fields)
+)]
+pub struct Ics20TransferDepositMemo {
+    /// the destination address for the deposit on the rollup
+    pub rollup_address: String,
 }
