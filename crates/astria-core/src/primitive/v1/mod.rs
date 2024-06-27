0f7faa802046c5314a423c20d037ebe8c4852189
noot <36753753+noot@users.noreply.github.com>
2024-06-27 15:27:21 -0400

    feat(core): implement with verification key for address builder and crypto improvements (#1218)
    ## Summary
    it's unclear/difficult of how an external user can construct an address
    given a verification key (i ran into this while updating hermes). this
    adds a `verification_key` method to the `Address::builder` to easily
    construct an address. also added `From` the ed25519 consensus types for
    the crypto module types.
    
    ## Background
    much better for ux, updating hermes would be painful without these.
    
    ## Changes
    - adds a `verification_key` method to the `Address::builder` to easily
    construct an address
    -  added `From` the ed25519 consensus types for the crypto module types.
    
    ## Testing
    n/a

--- crates/astria-core/src/primitive/v1/mod.rs
+++ crates/astria-core/src/primitive/v1/mod.rs
@@ -327,6 +327,23 @@ impl<TBytes, TPrefix> AddressBuilder<TBytes, TPrefix> {
         }
     }
 
+    /// Use the given verification key for address generation.
+    ///
+    /// The verification key is hashed with SHA256 and the first 20 bytes are used as the address
+    /// bytes.
+    #[allow(clippy::missing_panics_doc)] // allow clippy, as the conversion is infallible
+    #[must_use = "the builder must be built to construct an address to be useful"]
+    pub fn verification_key(
+        self,
+        key: &crate::crypto::VerificationKey,
+    ) -> AddressBuilder<WithBytes<'static>, TPrefix> {
+        let hash = Sha256::digest(key.as_bytes());
+        let array: [u8; ADDRESS_LEN] = hash[0..ADDRESS_LEN]
+            .try_into()
+            .expect("hash is 32 bytes long, so must always be able to convert to 20 bytes");
+        self.array(array)
+    }
+
     #[must_use = "the builder must be built to construct an address to be useful"]
     pub fn prefix<'a, T: Into<std::borrow::Cow<'a, str>>>(
         self,
