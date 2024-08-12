2750722ad13dd4e4663bc5bf9e192b1ed590f5ab
Bharath <vedabharath12345@gmail.com>
2024-08-12 11:59:37 +0530

    use bytes

--- crates/astria-core/src/generated/astria.composer.v1alpha1.rs
+++ crates/astria-core/src/generated/astria.composer.v1alpha1.rs
@@ -367,10 +367,10 @@ impl ::prost::Name for BuilderBundle {
 pub struct BuilderBundlePacket {
     #[prost(message, optional, tag = "1")]
     pub bundle: ::core::option::Option<BuilderBundle>,
-    #[prost(string, tag = "2")]
-    pub message_hash: ::prost::alloc::string::String,
-    #[prost(string, tag = "3")]
-    pub signature: ::prost::alloc::string::String,
+    #[prost(bytes = "vec", tag = "2")]
+    pub message_hash: ::prost::alloc::vec::Vec<u8>,
+    #[prost(bytes = "vec", tag = "3")]
+    pub signature: ::prost::alloc::vec::Vec<u8>,
 }
 impl ::prost::Name for BuilderBundlePacket {
     const NAME: &'static str = "BuilderBundlePacket";
