39578c08fdd8b360bd6c88987560c04c695627a1
quasystaty <ido@astria.org>
2024-08-13 16:44:40 +0300

    local deployment works

--- charts/evm-rollup/files/scripts/start-conductor.sh
+++ charts/evm-rollup/files/scripts/start-conductor.sh
@@ -2,19 +2,26 @@
 
 set -o errexit -o nounset -o pipefail
 
-# Request Celestia token if connecting to celestia
-BEARER_TOKEN=""
-if [ "$ASTRIA_CONDUCTOR_EXECUTION_COMMIT_LEVEL" != "SoftOnly" ]; then
-    BEARER_TOKEN=$(wget -qO- "$TOKEN_SERVER_URL")
+# Check if ASTRIA_CONDUCTOR_CELESTIA_BEARER_TOKEN is already defined
+if [ -z "${ASTRIA_CONDUCTOR_CELESTIA_BEARER_TOKEN:-}" ]; then
+    echo "ASTRIA_CONDUCTOR_CELESTIA_BEARER_TOKEN is not defined. Fetching the token..."
+    
+    # Request Celestia token if connecting to celestia
+    BEARER_TOKEN=""
+    if [ "$ASTRIA_CONDUCTOR_EXECUTION_COMMIT_LEVEL" != "SoftOnly" ]; then
+        BEARER_TOKEN=$(wget --timeout=10 --tries=1 -O - "$TOKEN_SERVER_URL")
 
-    if [ -z "$BEARER_TOKEN" ]; then
-        echo "Failed to fetch the Celestia bearer token."
-        exit 1
+        if [ -z "$BEARER_TOKEN" ]; then
+            echo "Failed to fetch the Celestia bearer token."
+            exit 1
+        fi
+
+        echo "Celestia Bearer token fetched successfully."
     fi
 
-    echo "Celestia Bearer token fetched successfully."
+    export ASTRIA_CONDUCTOR_CELESTIA_BEARER_TOKEN="$BEARER_TOKEN"
+else
+    echo "ASTRIA_CONDUCTOR_CELESTIA_BEARER_TOKEN is already defined. Skipping token fetch."
 fi
-
-export ASTRIA_CONDUCTOR_CELESTIA_BEARER_TOKEN="$BEARER_TOKEN"
 
 exec /usr/local/bin/astria-conductor
