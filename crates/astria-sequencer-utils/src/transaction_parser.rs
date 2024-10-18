6f90ae01cb2ea2b5183feb1cc349916aee99818e
Jordan Oroshiba <jordan@astria.org>
2024-10-18 15:42:41 -0700

    feat(sequencer-util): get json from transaction base64

---
+++ crates/astria-sequencer-utils/src/transaction_parser.rs
@@ -0,0 +1,81 @@
+use std::{
+    fs,
+    io,
+    path::{
+        Path,
+    },
+};
+
+use astria_eyre::eyre::{
+    Result,
+    WrapErr,
+};
+use base64::{
+    prelude::BASE64_STANDARD,
+    Engine,
+};
+use astria_core::{
+    generated::protocol::transaction::v1::Transaction as RawTransaction,
+    protocol::transaction::v1::Transaction,
+};
+
+#[derive(clap::Args, Debug)]
+pub struct Args {
+    /// Base64-encoded transaction data, or a file containing this, or stdin if `-`
+    #[arg(value_name = "TX|PATH")]
+    input: String,
+}
+
+/// Copies JSON application state from a file to a genesis JSON file,
+/// placing it at the key `app_state`.
+///
+/// # Errors
+///
+/// An `eyre::Result` is returned if either file cannot be opened,
+/// or if the destination genesis file cannot be saved.
+pub fn run(
+    Args {
+        input,
+    }: Args,
+) -> Result<()> {
+    let raw_transaction = parse(&input)?;
+    println!("Transaction:");
+    println!(
+        "{}",
+        serde_json::to_string_pretty(&raw_transaction).wrap_err("failed to json-encode")?
+    );
+    println!();
+    println!("Transaction Body:");
+    let transaction = Transaction::try_from_raw(raw_transaction).wrap_err("failed to convert to transaction")?;
+    println!("{}", serde_json::to_string_pretty(&transaction.unsigned_transaction().to_raw()).wrap_err("failed to json-encode")?);
+
+    Ok(())
+}
+
+fn parse(input: &str) -> Result<RawTransaction> {
+    use prost::Message;
+    let data = get_base64_data(input)?;
+    let transaction = RawTransaction::decode(&*data).wrap_err("failed to decode transaction")?;
+    Ok(transaction)
+}
+
+fn get_base64_data(input: &str) -> Result<Vec<u8>> {
+    if input == "-" {
+        let encoded = io::read_to_string(io::stdin().lock()).wrap_err("failed to read stdin")?;
+        return BASE64_STANDARD
+            .decode(encoded.trim())
+            .wrap_err("failed to decode stdin data as base64");
+    }
+
+    if Path::new(input).is_file() {
+        let encoded =
+            fs::read_to_string(input).wrap_err_with(|| format!("failed to read file `{input}`"))?;
+        return BASE64_STANDARD
+            .decode(encoded.trim())
+            .wrap_err_with(|| format!("failed to decode contents of `{input}` as base64"));
+    }
+
+    BASE64_STANDARD
+        .decode(input.trim())
+        .wrap_err("failed to decode provided blob data as base64")
+}
