43b9b4d425036418f83fd27b280995c4097433cb
Bharath <vedabharath12345@gmail.com>
2024-11-19 18:03:06 +0530

    rename optimistic_block to optimistic

--- crates/astria-sequencer/src/app/mod.rs
+++ crates/astria-sequencer/src/app/mod.rs
@@ -37,7 +37,7 @@ use astria_core::{
     },
     sequencerblock::v1::{
         block::SequencerBlock,
-        optimistic_block::SequencerBlockCommit,
+        optimistic::SequencerBlockCommit,
     },
     Protobuf as _,
 };
