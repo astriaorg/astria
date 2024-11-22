93a2d1ecc1c14db881f4a8c422b9e0f0fd1f2cb6
Richard Janis Goldschmidt <github@aberrat.io>
2024-11-22 06:47:55 +0100

    remove explicit retries for opening streams; rely on tonic's reconnection logic

--- crates/astria-auctioneer/src/optimistic_block_client.rs
+++ crates/astria-auctioneer/src/optimistic_block_client.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 use astria_core::{
     generated::sequencerblock::optimisticblock::v1alpha1::{
         optimistic_block_service_client::OptimisticBlockServiceClient,
@@ -19,15 +17,8 @@ use tonic::transport::{
     Endpoint,
     Uri,
 };
-use tracing::{
-    instrument,
-    warn,
-    Instrument as _,
-    Span,
-};
-use tryhard::backoff_strategies::ExponentialBackoff;
+use tracing::instrument;
 
-/// Wraps the gRPC client for the Sequencer service that wraps client calls with `tryhard`.
 #[derive(Debug, Clone)]
 pub(crate) struct OptimisticBlockClient {
     inner: OptimisticBlockServiceClient<Channel>,
@@ -49,7 +40,6 @@ impl OptimisticBlockClient {
         })
     }
 
-    // TODO: this should probably be separated from the tryhard logic and put in an extension trait
     #[instrument(skip_all, fields(
         uri = %self.uri,
         %rollup_id,
@@ -59,27 +49,17 @@ impl OptimisticBlockClient {
         &mut self,
         rollup_id: RollupId,
     ) -> eyre::Result<tonic::Streaming<GetOptimisticBlockStreamResponse>> {
-        let span = tracing::Span::current();
-        let retry_cfg = make_retry_cfg("stream optimistic blocks".into(), span);
-        let client = self.inner.clone();
-
-        let stream = tryhard::retry_fn(|| {
-            let mut client = client.clone();
-            let req = GetOptimisticBlockStreamRequest {
+        let stream = self
+            .inner
+            .get_optimistic_block_stream(GetOptimisticBlockStreamRequest {
                 rollup_id: Some(rollup_id.into_raw()),
-            };
-            async move { client.get_optimistic_block_stream(req).await }
-        })
-        .with_config(retry_cfg)
-        .in_current_span()
-        .await
-        .wrap_err("failed to initialize optimistic block stream")?
-        .into_inner();
-
+            })
+            .await
+            .wrap_err("failed to open optimistic block stream")?
+            .into_inner();
         Ok(stream)
     }
 
-    // TODO: this should probably be separated from the tryhard logic and put in an extension trait
     #[instrument(skip_all, fields(
            uri = %self.uri,
            err,
@@ -87,48 +67,12 @@ impl OptimisticBlockClient {
     pub(super) async fn get_block_commitment_stream(
         &mut self,
     ) -> eyre::Result<tonic::Streaming<GetBlockCommitmentStreamResponse>> {
-        let span = tracing::Span::current();
-        let retry_cfg = make_retry_cfg("stream block commitments".into(), span);
-        let client = self.inner.clone();
-
-        let stream = tryhard::retry_fn(|| {
-            let mut client = client.clone();
-            let req = GetBlockCommitmentStreamRequest {};
-            async move { client.get_block_commitment_stream(req).await }
-        })
-        .with_config(retry_cfg)
-        .in_current_span()
-        .await
-        .wrap_err("failed to initialize block commitment stream")?
-        .into_inner();
-
+        let stream = self
+            .inner
+            .get_block_commitment_stream(GetBlockCommitmentStreamRequest {})
+            .await
+            .wrap_err("failed to open block commitment stream")?
+            .into_inner();
         Ok(stream)
     }
-}
-
-fn make_retry_cfg(
-    msg: String,
-    span: Span,
-) -> tryhard::RetryFutureConfig<
-    ExponentialBackoff,
-    impl Fn(u32, Option<Duration>, &tonic::Status) -> futures::future::Ready<()>,
-> {
-    tryhard::RetryFutureConfig::new(1024)
-        .exponential_backoff(Duration::from_millis(100))
-        .max_delay(Duration::from_secs(2))
-        .on_retry(
-            move |attempt: u32, next_delay: Option<Duration>, error: &tonic::Status| {
-                let wait_duration = next_delay
-                    .map(humantime::format_duration)
-                    .map(tracing::field::display);
-                warn!(
-                    parent: &span,
-                    attempt,
-                    wait_duration,
-                    error = error as &dyn std::error::Error,
-                    "attempt to {msg} failed; retrying after backoff",
-                );
-                futures::future::ready(())
-            },
-        )
 }
--- crates/astria-auctioneer/src/optimistic_execution_client.rs
+++ crates/astria-auctioneer/src/optimistic_execution_client.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 use astria_core::{
     generated::bundle::v1alpha1::{
         optimistic_execution_service_client::OptimisticExecutionServiceClient,
@@ -17,13 +15,7 @@ use tonic::transport::{
     Endpoint,
     Uri,
 };
-use tracing::{
-    instrument,
-    warn,
-    Instrument,
-    Span,
-};
-use tryhard::backoff_strategies::ExponentialBackoff;
+use tracing::instrument;
 
 use crate::block::{
     self,
@@ -63,52 +55,13 @@ impl OptimisticExecutionClient {
         tonic::Streaming<ExecuteOptimisticBlockStreamResponse>,
         mpsc::Sender<block::Optimistic>,
     )> {
-        let span = tracing::Span::current();
-        let retry_cfg = make_retry_cfg("execute optimistic blocks".into(), span);
-        let client = self.inner.clone();
-
-        let (stream, opt_tx) = tryhard::retry_fn(|| {
-            let mut client = client.clone();
-
-            let (blocks_to_execute_tx, requests) = make_execution_requests_stream(rollup_id);
-
-            async move {
-                let stream = client.execute_optimistic_block_stream(requests).await?;
-                Ok((stream, blocks_to_execute_tx))
-            }
-        })
-        .with_config(retry_cfg)
-        .in_current_span()
-        .await
-        .wrap_err("failed to initialize optimistic execution stream")?;
-
-        Ok((stream.into_inner(), opt_tx))
+        let (blocks_to_execute_tx, requests) = make_execution_requests_stream(rollup_id);
+        let stream = self
+            .inner
+            .execute_optimistic_block_stream(requests)
+            .await
+            .wrap_err("failed to open execute optimistic block stream")?
+            .into_inner();
+        Ok((stream, blocks_to_execute_tx))
     }
-}
-
-fn make_retry_cfg(
-    msg: String,
-    span: Span,
-) -> tryhard::RetryFutureConfig<
-    ExponentialBackoff,
-    impl Fn(u32, Option<Duration>, &tonic::Status) -> futures::future::Ready<()>,
-> {
-    tryhard::RetryFutureConfig::new(1024)
-        .exponential_backoff(Duration::from_millis(100))
-        .max_delay(Duration::from_secs(2))
-        .on_retry(
-            move |attempt: u32, next_delay: Option<Duration>, error: &tonic::Status| {
-                let wait_duration = next_delay
-                    .map(humantime::format_duration)
-                    .map(tracing::field::display);
-                warn!(
-                    parent: &span,
-                    attempt,
-                    wait_duration,
-                    error = error as &dyn std::error::Error,
-                    "attempt to {msg} failed; retrying after backoff",
-                );
-                futures::future::ready(())
-            },
-        )
 }
