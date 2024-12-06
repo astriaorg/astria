dda57adcef51a2917ac8f2d62b9b18c63b7e2aff
ethanoroshiba <ethan@astria.org>
2024-12-06 10:40:04 -0600

    bump msrv

--- crates/astria-sequencer/src/mempool/mod.rs
+++ crates/astria-sequencer/src/mempool/mod.rs
@@ -112,7 +112,7 @@ struct ContainedTxLock<'a> {
     txs: RwLockWriteGuard<'a, HashSet<[u8; 32]>>,
 }
 
-impl<'a> ContainedTxLock<'a> {
+impl ContainedTxLock<'_> {
     fn add(&mut self, id: [u8; 32]) {
         if !self.txs.insert(id) {
             self.mempool.metrics.increment_internal_logic_error();
