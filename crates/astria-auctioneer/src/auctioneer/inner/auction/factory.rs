61052f67a7dd9396d48ddce11533a06beb9a9d4f
Richard Janis Goldschmidt <github@aberrat.io>
2025-01-08 15:31:26 +0100

    feat(auctioneer): add new service
    Co-authored-by: itamar <itamar.reif@gmail.com>

---
+++ crates/astria-auctioneer/src/auctioneer/inner/auction/factory.rs
@@ -0,0 +1,88 @@
+/// The auction Manager is responsible for managing running auction futures and their
+/// associated handles.
+use astria_core::{
+    primitive::v1::{
+        asset,
+        RollupId,
+    },
+    sequencerblock::v1::block::FilteredSequencerBlock,
+};
+use tokio::sync::{
+    mpsc,
+    oneshot,
+};
+use tokio_util::sync::CancellationToken;
+
+use super::{
+    Auction,
+    SequencerKey,
+    Worker,
+};
+use crate::sequencer_channel::SequencerChannel;
+
+pub(in crate::auctioneer::inner) struct Factory {
+    #[allow(dead_code)]
+    pub(in crate::auctioneer::inner) metrics: &'static crate::Metrics,
+    pub(in crate::auctioneer::inner) sequencer_abci_client: sequencer_client::HttpClient,
+    pub(in crate::auctioneer::inner) sequencer_channel: SequencerChannel,
+    pub(in crate::auctioneer::inner) latency_margin: std::time::Duration,
+    pub(in crate::auctioneer::inner) sequencer_key: SequencerKey,
+    pub(in crate::auctioneer::inner) fee_asset_denomination: asset::Denom,
+    pub(in crate::auctioneer::inner) sequencer_chain_id: String,
+    pub(in crate::auctioneer::inner) rollup_id: RollupId,
+    pub(in crate::auctioneer::inner) cancellation_token: CancellationToken,
+    /// `last_successful_nonce + 1` is used for submitting an auction winner to Sequencer
+    /// if an auction worker was not able to receive the last pending
+    /// nonce from Sequencer in time. Starts unset at the beginning of the program and
+    /// is set externally via Factory::set_last_succesful_nonce`.
+    pub(in crate::auctioneer::inner) last_successful_nonce: Option<u32>,
+}
+
+impl Factory {
+    pub(in crate::auctioneer::inner) fn start_new(
+        &self,
+        block: &FilteredSequencerBlock,
+    ) -> Auction {
+        let id = super::Id::from_sequencer_block_hash(block.block_hash());
+        let block_hash = *block.block_hash();
+        let height = block.height().into();
+
+        // TODO: get the capacities from config or something instead of using a magic number
+        let (start_bids_tx, start_bids_rx) = oneshot::channel();
+        let (start_timer_tx, start_timer_rx) = oneshot::channel();
+        let (bundles_tx, bundles_rx) = mpsc::unbounded_channel();
+
+        let cancellation_token = self.cancellation_token.child_token();
+        let auction = Worker {
+            sequencer_abci_client: self.sequencer_abci_client.clone(),
+            sequencer_channel: self.sequencer_channel.clone(),
+            start_bids: Some(start_bids_rx),
+            start_timer: Some(start_timer_rx),
+            bundles: bundles_rx,
+            latency_margin: self.latency_margin,
+            id,
+            sequencer_key: self.sequencer_key.clone(),
+            fee_asset_denomination: self.fee_asset_denomination.clone(),
+            sequencer_chain_id: self.sequencer_chain_id.clone(),
+            rollup_id: self.rollup_id,
+            cancellation_token: cancellation_token.clone(),
+            last_successful_nonce: self.last_successful_nonce,
+        };
+
+        Auction {
+            id,
+            block_hash,
+            height,
+            hash_of_executed_block_on_rollup: None,
+            start_bids: Some(start_bids_tx),
+            start_timer: Some(start_timer_tx),
+            bundles: bundles_tx,
+            cancellation_token,
+            worker: tokio::task::spawn(auction.run()),
+        }
+    }
+
+    pub(in crate::auctioneer::inner) fn set_last_successful_nonce(&mut self, nonce: u32) {
+        self.last_successful_nonce.replace(nonce);
+    }
+}
---
+++ crates/astria-auctioneer/src/bundle/mod.rs
@@ -0,0 +1,145 @@
+use astria_core::{
+    crypto::{
+        Signature,
+        VerificationKey,
+    },
+    generated::astria::bundle::v1alpha1 as raw,
+    primitive::v1::{
+        RollupId,
+        asset,
+    },
+    protocol::transaction::v1::{
+        TransactionBody,
+        action::RollupDataSubmission,
+    },
+    sequencerblock::v1::block,
+};
+use astria_eyre::eyre::{
+    self,
+    WrapErr as _,
+};
+use bytes::Bytes;
+use prost::Message as _;
+
+use crate::sequencer_key::SequencerKey;
+
+// TODO: this should probably be moved to astria_core::bundle?
+#[derive(Debug, Clone)]
+pub(crate) struct Bundle {
+    /// The fee that will be charged for this bundle
+    fee: u64,
+    /// The byte list of transactions fto be included.
+    transactions: Vec<Bytes>,
+    /// The hash of the rollup block that this bundle is based on.
+    // TODO: rename this to `parent_rollup_block_hash` to match execution api
+    prev_rollup_block_hash: [u8; 32],
+    /// The hash of the sequencer block used to derive the rollup block that this bundle is based
+    /// on.
+    base_sequencer_block_hash: block::Hash,
+}
+
+impl Bundle {
+    pub(crate) fn try_from_raw(raw: raw::Bundle) -> eyre::Result<Self> {
+        let raw::Bundle {
+            fee,
+            transactions,
+            base_sequencer_block_hash,
+            prev_rollup_block_hash,
+        } = raw;
+        Ok(Self {
+            fee,
+            transactions,
+            prev_rollup_block_hash: prev_rollup_block_hash
+                .as_ref()
+                .try_into()
+                .wrap_err("invalid prev_rollup_block_hash")?,
+            base_sequencer_block_hash: base_sequencer_block_hash
+                .as_ref()
+                .try_into()
+                .wrap_err("invalid base_sequencer_block_hash")?,
+        })
+    }
+
+    fn into_raw(self) -> raw::Bundle {
+        raw::Bundle {
+            fee: self.fee,
+            transactions: self.transactions,
+            base_sequencer_block_hash: Bytes::copy_from_slice(
+                self.base_sequencer_block_hash.as_bytes(),
+            ),
+            prev_rollup_block_hash: Bytes::copy_from_slice(&self.prev_rollup_block_hash),
+        }
+    }
+
+    pub(crate) fn into_transaction_body(
+        self,
+        nonce: u32,
+        rollup_id: RollupId,
+        sequencer_key: &SequencerKey,
+        fee_asset: asset::Denom,
+        chain_id: String,
+    ) -> TransactionBody {
+        let allocation = Allocation::new(self, sequencer_key);
+        let allocation_data = allocation.into_raw().encode_to_vec();
+
+        TransactionBody::builder()
+            .actions(vec![
+                RollupDataSubmission {
+                    rollup_id,
+                    data: allocation_data.into(),
+                    fee_asset,
+                }
+                .into(),
+            ])
+            .nonce(nonce)
+            .chain_id(chain_id)
+            .try_build()
+            .expect("failed to build transaction body")
+    }
+
+    pub(crate) fn bid(&self) -> u64 {
+        self.fee
+    }
+
+    pub(crate) fn parent_rollup_block_hash(&self) -> [u8; 32] {
+        self.prev_rollup_block_hash
+    }
+
+    pub(crate) fn base_sequencer_block_hash(&self) -> &block::Hash {
+        &self.base_sequencer_block_hash
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct Allocation {
+    signature: Signature,
+    verification_key: VerificationKey,
+    payload: Bundle,
+}
+
+impl Allocation {
+    fn new(bundle: Bundle, sequencer_key: &SequencerKey) -> Self {
+        let bundle_data = bundle.clone().into_raw().encode_to_vec();
+        let signature = sequencer_key.signing_key().sign(&bundle_data);
+        let verification_key = sequencer_key.signing_key().verification_key();
+        Self {
+            signature,
+            verification_key,
+            payload: bundle,
+        }
+    }
+
+    fn into_raw(self) -> raw::Allocation {
+        let Self {
+            signature,
+            verification_key,
+            payload,
+        } = self;
+
+        raw::Allocation {
+            signature: Bytes::copy_from_slice(&signature.to_bytes()),
+            public_key: Bytes::copy_from_slice(&verification_key.to_bytes()),
+            payload: Some(payload.into_raw()),
+        }
+    }
+}
