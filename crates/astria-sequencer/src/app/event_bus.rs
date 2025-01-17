c5d6f9c98dd21b6703c988aaae44fb27a41713cd
Richard Janis Goldschmidt <github@aberrat.io>
2025-01-17 13:50:58 +0100

    fix module name in  changelog, turn comments into doc comments, tighten visibility, remove unused traits

--- crates/astria-sequencer/src/app/event_bus.rs
+++ crates/astria-sequencer/src/app/event_bus.rs
@@ -14,13 +14,13 @@ use tokio_util::sync::CancellationToken;
 /// `receive` method.
 #[derive(Clone)]
 pub(crate) struct EventReceiver<T> {
-    // The receiver side of the watch which is read for the latest value of the event.
-    // We receive an Option over T because the sender side of the watch is designed to send
-    // Option values. This allows the sender value to send objects which do not have a `Default`
-    // implementation.
+    /// The receiver side of the watch which is read for the latest value of the event.
+    /// We receive an Option over T because the sender side of the watch is designed to send
+    /// Option values. This allows the sender value to send objects which do not have a `Default`
+    /// implementation.
     inner: Receiver<Option<T>>,
-    // To signal subscribers that the event bus is initialized, i.e. that the value in `sender` was
-    // set.
+    /// To signal subscribers that the event bus is initialized, i.e. that the value in `sender`
+    /// was set.
     is_init: CancellationToken,
 }
 
@@ -28,14 +28,15 @@ impl<T> EventReceiver<T>
 where
     T: Clone,
 {
-    // Marks the current message in the receiver end of the watch as seen.
-    // This is useful in situations where we want to ignore the current value of the watch
-    // and wait for the next value.
+    /// Marks the current message in the receiver end of the watch as seen.
+    ///
+    /// This is useful in situations where we want to ignore the current value of the watch
+    /// and wait for the next value.
     pub(crate) fn mark_latest_event_as_seen(&mut self) {
         self.inner.mark_unchanged();
     }
 
-    // Returns the latest value of the event, waiting for the value to change if it hasn't already.
+    /// Returns the latest value of the event, waiting for the value to change if it hasn't already.
     pub(crate) async fn receive(&mut self) -> astria_eyre::Result<T> {
         // This will get resolved when the sender side of the watch is initialized by sending
         // the first value. We wait till the sender side of the watch has initialized the watch.
@@ -54,7 +55,6 @@ where
 }
 
 /// `EventSender` contains the sender side of the events sent by the Sequencer App.
-/// At any given time, it sends the latest value of the event.
 struct EventSender<T> {
     // A watch channel that is always starts unset. Once set, the `is_init` token is cancelled
     // and value in the channel will never be unset.
@@ -65,6 +65,7 @@ struct EventSender<T> {
 }
 
 impl<T> EventSender<T> {
+    /// Create a new sender for an event `T`.
     fn new() -> Self {
         let (sender, _) = tokio::sync::watch::channel(None);
         Self {
@@ -73,8 +74,7 @@ impl<T> EventSender<T> {
         }
     }
 
-    // Returns a `EventReceiver` object that contains the receiver side of the watch which can be
-    // used to receive the latest value of the event.
+    /// Creates a receiver for events `T`.
     fn subscribe(&self) -> EventReceiver<T> {
         EventReceiver {
             inner: self.inner.subscribe(),
@@ -82,7 +82,7 @@ impl<T> EventSender<T> {
         }
     }
 
-    // Sends the event to all the subscribers.
+    /// Sends the event to all subscribers.
     fn send(&self, event: T) {
         self.inner.send_replace(Some(event));
         // after sending the first value, we resolve the is_init token to signal that the sender
@@ -92,20 +92,21 @@ impl<T> EventSender<T> {
     }
 }
 
-/// `EventBusSubscription` contains [`EventReceiver`] of various events that can be subscribed.
-/// It can be cloned by various components in the sequencer app to receive events.
-#[derive(Clone)]
+/// A subscription to the event bus.
+///
+/// Allows subscribing to specific events like [`Self::process_proposal_blocks`]
+/// and [`Self::finalized_blocks`].
 pub(crate) struct EventBusSubscription {
     process_proposal_blocks: EventReceiver<Arc<SequencerBlock>>,
     finalized_blocks: EventReceiver<Arc<FinalizeBlock>>,
 }
 
 impl EventBusSubscription {
-    pub(crate) fn process_proposal_blocks(&mut self) -> EventReceiver<Arc<SequencerBlock>> {
+    pub(crate) fn process_proposal_blocks(&self) -> EventReceiver<Arc<SequencerBlock>> {
         self.process_proposal_blocks.clone()
     }
 
-    pub(crate) fn finalized_blocks(&mut self) -> EventReceiver<Arc<FinalizeBlock>> {
+    pub(crate) fn finalized_blocks(&self) -> EventReceiver<Arc<FinalizeBlock>> {
         self.finalized_blocks.clone()
     }
 }
@@ -127,6 +128,7 @@ pub(super) struct EventBus {
 }
 
 impl EventBus {
+    /// Instantiates a new event bus.
     pub(super) fn new() -> Self {
         let process_proposal_block_sender = EventSender::new();
         let finalized_block_sender = EventSender::new();
@@ -137,21 +139,20 @@ impl EventBus {
         }
     }
 
-    // Returns a `EventBusSubscription` object that contains receivers of various events that can
-    // be subscribed to.
-    pub(crate) fn subscribe(&self) -> EventBusSubscription {
+    /// Subscribe to the event bus.
+    pub(super) fn subscribe(&self) -> EventBusSubscription {
         EventBusSubscription {
             process_proposal_blocks: self.process_proposal_block_sender.subscribe(),
             finalized_blocks: self.finalized_block_sender.subscribe(),
         }
     }
 
-    // Sends a process proposal block event to the subscribers.
+    /// Sends a process proposal block event over the event bus.
     pub(super) fn send_process_proposal_block(&self, sequencer_block: Arc<SequencerBlock>) {
         self.process_proposal_block_sender.send(sequencer_block);
     }
 
-    // Sends a finalized block event to the subscribers.
+    /// Sends a finalized block event over the event bus.
     pub(super) fn send_finalized_block(&self, sequencer_block_commit: Arc<FinalizeBlock>) {
         self.finalized_block_sender.send(sequencer_block_commit);
     }
