7450f84bf57b1028bec6c0594512e60ee0f72ae4
Joshua Dechant <josh@codecrafting.org>
2025-01-31 12:46:45 -0500

    feat(charts): Snapshots for geth nodes (#1906)
    ## Summary
    Allows restoring a geth node from a snapshot, and creating snapshots on
    a schedule with the option to upload them to remote storage via rclone.
    
    ## Background
    Spinning up a geth node from a snapshot is more ideal than syncing from
    genesis. We won't always have a PVC snapshot to restore from. This
    allows any geth node to migrate to Astria infra, and is needed to
    migrate Forma testnet.
    
    ## Changes
    - Added a restore from snapshot
    - snapshots can be loaded from any compatible rclone source (including
    local)
      - rclone config allow snapshots to be loaded from private sources
      - optional sha256 checksum to verify the snapshot after download
    - Added scheduled snapshot creation
      - Cron schedule
      - Retention count for # of snapshots to keep on local  disk
      - Optional upload of snapshot to a remote destination
    - rclone config management via secrets
    
    ## Testing
    Tested on local k8s dev cluster, with and without rclone w/ an AWS S3
    config

---
+++ charts/evm-rollup/files/scripts/restore-snapshot.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -o errexit -o nounset
+
+rm -rf "$data_dir/geth"
+mkdir -p "$data_dir/geth"
+
+SNAPSHOT="{{ .Values.config.geth.snapshot.restore.source }}"
+SNAPSHOT_FILE=$(basename "$SNAPSHOT")
+
+echo "💿 Copying snapshot from $SNAPSHOT"
+
+rclone copy -vv \
+  {{ .Values.config.geth.snapshot.restore.source }} \
+  "$data_dir/snapshot-load/"
+
+{{if .Values.config.geth.snapshot.restore.checksum -}}
+echo "🕵️ Verifying snapshot checksum..."
+EXPECTED_CHECKSUM="{{ .Values.config.geth.snapshot.restore.checksum }}"
+ACTUAL_CHECKSUM=$(sha256sum "$data_dir/snapshot-load/$SNAPSHOT_FILE" | cut -d ' ' -f 1)
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]; then
+  echo "🚨 Checksum verification failed!"
+  echo "Expected: $EXPECTED_CHECKSUM"
+  echo "Got: $ACTUAL_CHECKSUM"
+  exit 1
+fi
+echo "✅ Checksum verified successfully"
+{{- end}}
+
+echo "Extracting snapshot..."
+tar -xvf $data_dir/snapshot-load/$SNAPSHOT_FILE -C $data_dir/geth
+
+echo "🧹 Cleaning up..."
+rm -rf $data_dir/snapshot-load
+
+echo "Snapshot loaded successfully 🎉"
