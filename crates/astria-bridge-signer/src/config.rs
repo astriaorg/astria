fb4f8cb8f99bdb1ca856b1b2deabb4731ad05d57
elizabeth <elizabethjbinks@gmail.com>
2025-02-06 17:14:33 -0500

    wip start tx verifier

--- crates/astria-bridge-signer/src/config.rs
+++ crates/astria-bridge-signer/src/config.rs
@@ -5,13 +5,13 @@ use serde::{
 
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 pub struct Config {
-    // The address of the grpc endpoint.
+    /// The address of the grpc endpoint.
     pub grpc_endpoint: String,
-    // The path to the json-encoded frost public key package.
-    pub frost_public_key_package_path: String,
-    // The path to the json-encoded frost secret key package.
+    /// The path to the json-encoded frost secret key package.
     pub frost_secret_key_package_path: String,
-    // Log filter directives.
+    /// Rollup EVM node RPC endpoint.
+    pub rollup_rpc_endpoint: String,
+    /// Log filter directives.
     pub log: String,
     /// Forces writing trace data to stdout no matter if connected to a tty or not.
     pub force_stdout: bool,
