d202226559aca10ad92a396f91b14a996dea311b
ido <ido@astria.org>
2025-02-10 14:21:30 +0200

    geth flags patch

--- charts/flame-rollup/files/scripts/init-geth.sh
+++ charts/flame-rollup/files/scripts/init-geth.sh
@@ -8,11 +8,15 @@ if [ ! -d "$data_dir/" ]; then
   cp /scripts/geth-genesis.json $home_dir/genesis.json
 
   exec geth \
-    {{- range $arg := .Values.config.geth.flags -}}
-    {{- if $arg.condition | default true -}}
-    --{{ $arg.name }}{{ if $arg.value }}={{ tpl $arg.value $ }}{{ end }} \
-    {{ end }}
-    {{- end -}}
+    {{- range $arg := .Values.config.geth.flags }}
+    {{- if hasKey $arg "condition" }}
+    {{- if eq (tpl $arg.condition $) "true" }}
+    --{{ $arg.name }}{{ if $arg.value }}={{ tpl $arg.value $ }}{{ end }}
+    {{- end }}
+    {{- else }}
+    --{{ $arg.name }}{{ if $arg.value }}={{ tpl $arg.value $ }}{{ end }}
+    {{- end }}
+    {{- end }}
     init $home_dir/genesis.json
 elif ! cmp -s "/scripts/geth-genesis.json" "$home_dir/genesis.json"; then
   echo "Geth DB already initialized, but genesis file upgraded..."
