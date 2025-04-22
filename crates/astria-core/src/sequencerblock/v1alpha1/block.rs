2feb7a1681b561a94cdb84c6871341254dcbb201
ethanoroshiba <ethan@astria.org>
2025-04-22 13:48:45 -0500

    feature gate serde derive

--- crates/astria-core/src/sequencerblock/v1alpha1/block.rs
+++ crates/astria-core/src/sequencerblock/v1alpha1/block.rs
@@ -1360,8 +1360,12 @@ impl FilteredSequencerBlockError {
 ///
 /// A [`Deposit`] is constructed whenever a [`BridgeLockAction`] is executed
 /// and stored as part of the block's events.
-#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize)]
-#[serde(into = "crate::generated::astria::sequencerblock::v1alpha1::Deposit")]
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize),
+    serde(into = "crate::generated::astria::sequencerblock::v1alpha1::Deposit")
+)]
 pub struct Deposit {
     // the address on the sequencer to which the funds were sent to.
     pub bridge_address: Address,
